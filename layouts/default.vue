dc3fb9efde18cb2382b8dde1ab3d599fd7f828d4
August S. Freytag <night@apricummedia.com>
2019-02-27 16:06:50 +0100

    Add default layout override file without contents

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+	<nuxt />
+</template>
+
+<style lang="scss">
+</style>
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator"
+
+	@Component
+	export default class HeaderComponent extends Vue {}
+</script>
+
