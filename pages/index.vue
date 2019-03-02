999e72faaa3b7275fa9b519df8029b3f84bf2869
August Freytag <night@apricummedia.com>
2019-03-02 10:20:13 +0100

    Rearrange and update index page component, script and stylesheet

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,21 @@
 <template>
-	<div class="case">
-		<div class="wrap">
-			<header>
-				<div class="branding">
-					<h1>{{ content.headerPrimaryTitle }}</h1>
-					<section class="tagline">{{ content.headerPrimaryOccupations }}</section>
-				</div>
-				<div>
-					<nav>
-					</nav>
-				</div>
-			</header>
-			<main>
-				<section class="box-couple" v-for="index in 5" :key="index">
-					<div>
-						<h2>
-							<div class="content-primer">Showcase:</div>
-							<div class="content-title">"There I Was"</div>
-						</h2>
-						<div>
-							<p>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						</div>
-					</div>
-					<div>CONTENT!</div>
-				</section>
-			</main>
-			<footer>
-				Footer here.
-			</footer>
+	<span>
+		<section class="box-couple" v-for="index in 5" :key="index">
+		<div>
+			<h2>
+				<div class="content-primer">Showcase:</div>
+				<div class="content-title">"There I Was"</div>
+			</h2>
+			<div>
+				<p>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
 		</div>
-	</div>
+		<div>CONTENT!</div>
+	</section>
+	</span>
 </template>
 
-<style lang="scss">
-	@import "~/assets/style/fonts.scss";
-	@import "./index.scss";
+<style lang="scss" src="./index-page.scss">
 </style>
 
 <script lang="ts">
@@ -44,14 +25,6 @@
 	@Component({
 		components: {
 			HeaderComponent
-		},
-		async asyncData() {
-			return {
-				content: {
-					headerPrimaryTitle: "August S. Freytag",
-					headerPrimaryOccupations: ["Music Video Artist", "Editor", "Concept Designer", "Developer", "Analogue Photographer"].join(" ・ ")
-				}
-			}
 		}
 	})
 	export default class IndexPage extends Vue {}
