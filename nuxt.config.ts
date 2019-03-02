7809947232da669eae9cb243fe209da342287d60
August Freytag <night@apricummedia.com>
2019-03-02 10:24:41 +0100

    Add source map generation, switch to SPA mode during development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 export default {
 	dev: true,
-	buildDir: "/var/lib/nuxt/build"
+	mode: "spa",
+	buildDir: "/var/lib/nuxt/build",
+	build: {
+		extend (config: any, keys: any) {
+			const isClient = keys.isClient as Boolean
+			
+			if (isClient) {
+				config.devtool = "eval-source-map"
+			}
+		}
+	}
 }
