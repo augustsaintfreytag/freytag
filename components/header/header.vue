8b762a4641c2d30e607c685e30891c25d5631c41
August Freytag <night@apricummedia.com>
2019-03-02 10:18:12 +0100

    Introduce separate header vue component and stylesheet

---
+++ components/header/header.vue
@@ -0,0 +1,30 @@
+<template>
+	<header>
+		<div class="branding">
+			<h1>{{ content.headerPrimaryTitle }}</h1>
+			<section class="tagline">{{ content.headerPrimaryOccupations }}</section>
+		</div>
+		<div>
+			<nav>
+			</nav>
+		</div>
+	</header>
+</template>
+
+<style lang="scss" src="./header.scss"></style>
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator"
+
+	@Component
+	export default class HeaderComponent extends Vue {
+		data() {
+			return {
+				content: {
+					headerPrimaryTitle: "August S. Freytag",
+					headerPrimaryOccupations: ["Music Video Artist", "Editor", "Concept Designer", "Developer", "Analogue Photographer"].join(" ・ ")
+				}
+			}
+		}
+	}
+</script>
