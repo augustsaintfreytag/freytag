427ecfb90750fc86a8b24b0a3de5fcdc26cef0e7
August S. Freytag <night@apricummedia.com>
2019-03-09 12:03:04 +0100

    Establish changed layout with dual-row structure for life event page

--- pages/life/index.vue
+++ pages/life/index.vue
@@ -2,12 +2,17 @@
 	<section class="life-page inset">
 		<ul class="life-events">
 			<li class="life-event" v-for="vitaEvent in vitaEvents" :key="vitaEvent.index">
-				<div class="blip"></div>
-				<h2 class="event-text">{{ vitaEvent.name }}</h2>
-				<div class="event-text time">{{ formattedDateRange(vitaEvent) }}</div>
-				<!--<div class="spacer"></div>-->
-				<div class="event-text format">{{ vitaEvent.format }}</div>
-				<div class="event-text role">{{ vitaEvent.role }}</div>
+				<div class="header">
+					<div class="blip"></div>
+					<h2 class="name">{{ vitaEvent.name }}</h2>
+				</div>
+				<div class="details">
+					<div class="time">{{ formattedDateRange(vitaEvent) }}</div>
+					<div class="format">{{ vitaEvent.format }}</div>
+					<div class="role">{{ vitaEvent.role }}</div>
+					<div class="location">{{ vitaEvent.location }}</div>
+					<div class="context">{{ vitaEvent.context }}</div>
+				</div>
 			</li>
 		</ul>
 	</section>
