5783ffe3adee4f6be88a85fd55e875fc20e3440c
August S. Freytag <night@apricummedia.com>
2019-03-14 22:26:42 +0100

    Establish base layout for grid element in work overview with sheet add.

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,5 +1,19 @@
 <template>
-	<span>OVERVIEW PAGE</span>
+	<section class="work-overview-page">
+		<section class="work-overview inset">
+			<section class="work-item kind-blip-colored kind-all">
+				<div class="image-holder"></div>
+				<div class="item-data">
+					<div class="blip kind-life"></div>
+					<div class="format">Photographic Series</div>
+					<div class="contents">7 samples</div>
+				</div>
+				<div class="item-name">
+					<h2>There I Was (Children in Suits)</h2>
+				</div>
+			</section>
+		</section>
+	</section>
 </template>
 
 <style lang="scss" src="./work-overview-page.scss"></style>
