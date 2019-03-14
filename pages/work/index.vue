38c238e9417cc42bc4f09ddcf197fcc9f7af4e38
August S. Freytag <night@apricummedia.com>
2019-03-14 15:09:18 +0100

    Introduce work detail page template, styles and placeholder component
    Add header section with title image holder, title text and scrolling indicator
    with central alignment.

---
+++ pages/work/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<section class="work-page">
+		<section class="introducer">
+			<div class="image-holder"></div>
+			<div class="title-holder">
+				<h2>Writers United and Associated</h2>
+			</div>
+			<div class="scroll-indicator">
+				<svg class="icon">
+					<use xlink:href="#scroll-indicator"></use>
+				</svg>
+			</div>
+		</section>
+	</section>
+</template>
+
+<style lang="scss" src="./work-page.scss"></style>
+
+<script lang="ts" src="./work-page.ts"></script>
