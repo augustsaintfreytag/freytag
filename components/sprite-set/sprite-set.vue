7382ddd32b292b6cef30e8e5acd8cdf185d6c829
August S. Freytag <night@apricummedia.com>
2019-03-14 15:08:21 +0100

    Introduce scroll indicator to sprite set, intended for work detail

--- components/sprite-set/sprite-set.vue
+++ components/sprite-set/sprite-set.vue
@@ -1,10 +1,13 @@
 <template>
-	<svg width="0" height="0" class="hidden">
-		<symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 20" id="sorting-pop-up-button">
-			<path d="M0 10.9h12l-6 8.4-6-8.4zm12-2.2H0L6 .3l6 8.4z"></path>
-		</symbol>
-		<symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.979 8.447" id="sorting-switch">
-			<path d="M0 0h11.979L5.99 8.447z" fill="#a7a7a7"></path>
-		</symbol>
-	</svg>
+<svg width="0" height="0" class="hidden">
+  <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300" id="scroll-indicator">
+    <path d="M255.3 94.1c-3.5-3.5-9.2-3.5-12.7 0L150 186.8 57.4 94.1c-3.5-3.5-9.2-3.5-12.7 0s-3.5 9.2 0 12.7l99 99c1.8 1.8 4.1 2.6 6.4 2.6 2.3 0 4.6-.9 6.4-2.6l99-99c3.4-3.4 3.4-9.1-.2-12.7z"></path>
+  </symbol>
+  <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 20" id="sorting-pop-up-button">
+    <path d="M0 10.9h12l-6 8.4-6-8.4zm12-2.2H0L6 .3l6 8.4z"></path>
+  </symbol>
+  <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.979 8.447" id="sorting-switch">
+    <path d="M0 0h11.979L5.99 8.447z" fill="#a7a7a7"></path>
+  </symbol>
+</svg>
 </template>
