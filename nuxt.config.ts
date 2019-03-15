531ea7fff026fc10feffdc5457dc82335a510b0e
August S. Freytag <night@apricummedia.com>
2019-03-15 11:56:34 +0100

    Fix boolean type cast used in nuxt base configuration file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default {
 	buildDir: "/var/lib/nuxt/build",
 	build: {
 		extend (config: any, keys: any) {
-			const isClient = keys.isClient as Boolean
+			const isClient = keys.isClient as boolean
 			
 			if (isClient) {
 				config.devtool = "eval-source-map"
