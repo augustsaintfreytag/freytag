9ebf0316fb4d36ff057b3c2f5b640bfa4dddff82
August S. Freytag <night@apricummedia.com>
2019-03-15 15:07:29 +0100

    Introduce definitions for content block collection entries and models

---
+++ components/common/cockpit/models/content-block.ts
@@ -0,0 +1,91 @@
+import { CockpitEntry } from "./cockpit-response"
+import { Url } from "../../library/url"
+
+export namespace Content {
+
+	// Live Model
+
+	export interface Block {
+
+		form: Form|undefined
+
+	}
+
+	export class TextQuoteBlock implements Block {
+
+		form: Form|undefined
+		textContent: string|undefined
+
+		constructor(entry: BlockEntry) {
+			this.form = (entry.form as Form) || undefined
+			this.textContent = entry.textContent || undefined
+		}
+
+	}
+
+	export class TextColumnBlock extends TextQuoteBlock {}
+
+	export class ImageColumnsBlock implements Block {
+
+		form: Form|undefined
+		imageContents: Image.Content[]
+
+		constructor(entry: BlockEntry) {
+			if (entry.form !== Form.ImageColumns) {
+				throw new TypeError(`Entry has incompatible form '${entry.form}', expected image columns block.`)
+			}
+
+			this.form = (entry.form as Form) || undefined
+			this.imageContents = (entry.imageContents || []).map(imageContentEntry => {
+				return new Image.Content(imageContentEntry)
+			})
+		}
+
+	}
+
+	export enum Form {
+
+		TextQuote = "Text (Quote)",
+		TextColumn = "Text (Column)",
+		ImageColumns = "Images (Columns)",
+		VideoVimeo = "Video (Vimeo)"
+
+	}
+
+	// Stored Model
+
+	export interface BlockEntry extends CockpitEntry {
+
+		form: string
+		identifierItem: string
+		identifierGroup: string
+		textContent: string
+		imageContents: Image.ContentEntry[]|undefined
+		videoCode: string|undefined
+
+	}
+
+	// Image Submodel
+
+	export namespace Image {
+
+		export interface ContentEntry {
+
+			field: {type: string, label: string}
+			value: {path: string}
+	
+		}
+
+		export class Content {
+
+			path: Url|undefined
+			
+			constructor(entry: ContentEntry) {
+				this.path = entry.value.path || undefined
+			}
+
+		}
+
+	}
+
+}
