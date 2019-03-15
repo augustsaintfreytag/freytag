ab03a0d716763162ec0d04cafd1961fcd03e3e18
August S. Freytag <night@apricummedia.com>
2019-03-15 23:38:11 +0100

    Update readout and instantiation of image content and entry models

--- components/common/cockpit/models/image.ts
+++ components/common/cockpit/models/image.ts
@@ -2,20 +2,23 @@ import { Url } from "../../library/url"
 
 export namespace Image {
 
-	export interface ContentEntry {
-
-		field: {type: string, label: string}
-		value: {path: string}
-
-	}
+	// Live Model
 
 	export class Content {
 
 		path: Url|undefined
 		
 		constructor(entry: ContentEntry) {
-			this.path = entry.value.path || undefined
+			this.path = entry.path || undefined
 		}
+
+	}
+
+	// Stored Model
+
+	export interface ContentEntry {
+
+		path: string
 
 	}
 
