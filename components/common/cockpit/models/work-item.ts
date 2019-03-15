bdfc381d403e3a57806d13efbc6e95d7959b7979
August S. Freytag <night@apricummedia.com>
2019-03-15 15:45:13 +0100

    Introduce data model namespace for work item and related elements

---
+++ components/common/cockpit/models/work-item.ts
@@ -0,0 +1,73 @@
+import { CockpitEntry } from "./cockpit-response"
+import { Vita } from "./vita-event"
+import { Image } from "./image"
+import { Content } from "./content-block";
+
+export namespace Work {
+
+	// Live Model
+
+	export class Item {
+
+		display: boolean
+		name: string
+		description: string|undefined
+		titleImage: Image.Content|undefined
+		event: Vita.Event|undefined
+		blocks: Content.Block[]
+
+		constructor(entry: ItemEntry) {
+			this.display = entry.display
+			this.name = entry.name
+			this.description = entry.description || undefined
+			
+			if (entry.titleImage) {
+				this.titleImage = new Image.Content(entry.titleImage)
+			}
+
+			if (entry.event) {
+				this.event = new Vita.Event(entry.event)
+			}
+			
+			this.blocks = (entry.blocks || []).map(contentBlockEntry => {
+				switch (contentBlockEntry.form) {
+					case Content.Form.TextQuote:
+						return new Content.TextQuoteBlock(contentBlockEntry)
+					case Content.Form.TextColumn:
+						return new Content.TextColumnBlock(contentBlockEntry)
+					case Content.Form.ImageColumns:
+						return new Content.ImageColumnsBlock(contentBlockEntry)
+					case Content.Form.VideoVimeo:
+						return new Content.VideoVimeoBlock(contentBlockEntry)
+					default:
+						return undefined
+				}
+			}).filter(model => { return model !== undefined }) as Content.Block[]
+		}
+
+	}
+
+	type AnyClass = { new(...args: any[]): any }
+
+	function instantiatedModel<T extends AnyClass>(Model: T, entry: any|undefined): T|undefined {
+		if (!entry) {
+			return undefined
+		}
+
+		return new Model(entry)
+	}
+
+	// Stored Model
+
+	export interface ItemEntry extends CockpitEntry {
+
+		display: boolean
+		name: string
+		description: string|undefined
+		titleImage: Image.ContentEntry|undefined
+		event: Vita.EventEntry|undefined
+		blocks: Content.BlockEntry[]|undefined
+
+	}
+
+}
