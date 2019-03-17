996d4047ee5fd3b3ce89f35ed12d118941ede229
August S. Freytag <night@apricummedia.com>
2019-03-17 14:42:47 +0100

    Introduce column text block component, styles, and logic

---
+++ components/content-blocks/text-column-block/text-column-block.vue
@@ -0,0 +1,9 @@
+<template>
+	<section class="content-block text-column-block inset" v-if="contentBlock">
+		<p>{{ contentBlock.textContent }}</p>
+	</section>
+</template>
+
+<style lang="scss" src="./text-column-block.scss"></style>
+
+<script lang="ts" src="./text-column-block.ts"></script>
