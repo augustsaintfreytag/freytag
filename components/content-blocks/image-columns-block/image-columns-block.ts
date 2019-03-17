06f7180895c322a1f4c56987037ebe688831f172
August S. Freytag <night@apricummedia.com>
2019-03-17 14:09:48 +0100

    Introduce component and script definition for image columns content blk.

---
+++ components/content-blocks/image-columns-block/image-columns-block.ts
@@ -0,0 +1,30 @@
+import { Component, Vue } from "vue-property-decorator"
+import { Content } from "~/components/common/storage/models/content-block";
+
+@Component({
+
+	props: ["contentBlock"],
+
+	computed: {
+		imageContentDesignation(): string {
+			const contentBlock = this.$props.contentBlock as Content.ImageColumnsBlock
+			const numberOfImageContents = contentBlock.imageContents.length
+
+			if (numberOfImageContents === 1) {
+				return "single"
+			}
+
+			if (numberOfImageContents === 2) {
+				return "dual"
+			}
+
+			if (numberOfImageContents >= 3) {
+				return "full"
+			}
+
+			return "none"
+		}
+	}
+
+})
+export default class ImageColumnsBlockComponent extends Vue {}
