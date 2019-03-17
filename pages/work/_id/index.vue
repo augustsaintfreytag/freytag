2234f478990af8de6a5a714ad72cd32ec0b4077b
August S. Freytag <night@apricummedia.com>
2019-03-17 11:37:02 +0100

    Establish work detail page component and stylesheet

--- pages/work/_id/index.vue
+++ pages/work/_id/index.vue
@@ -1,9 +1,11 @@
 <template>
 	<section class="work-pages work-detail-page">
-		<section class="introducer">
-			<div class="image-holder"></div>
+		<section class="cover">
+			<div class="image-holder">
+				<img v-if="workItem.titleImage" v-bind:src="$root.managedResourcePath(workItem.titleImage.path)" />
+			</div>
 			<div class="title-holder">
-				<h2>Writers United and Associated</h2>
+				<h2>{{ workItem.name }}</h2>
 			</div>
 			<div class="scroll-indicator">
 				<svg class="icon">
@@ -11,6 +13,22 @@
 				</svg>
 			</div>
 		</section>
+		<section class="introduction inset">
+			<section class="details">
+				<table>
+					<tbody>
+						<tr v-for="row in detailsRows" :key="row.name">
+							<td class="label">{{ row.name }}</td>
+							<td class="text">{{ row.value }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</section>
+			<section class="description">
+				{{ workItem.description }}
+			</section>
+		</section>
+		<image-columns-block-component />
 	</section>
 </template>
 
