f4491709f7e866fea2329e0cc0e4178aa409bbc0
August S. Freytag <night@apricummedia.com>
2019-03-18 15:12:08 +0100

    Establish updated provisioning scripts for live with separate env file

--- server/provision-live.sh
+++ server/provision-live.sh
@@ -1,7 +1,7 @@
 source ./cycle-live.env
-export NGINX_CONF_NAME="nginx.dev.conf"
+source ./provision-live.env
 
-docker-compose up -d proxy
-docker-compose exec certbot /bin/bash -c "run -d augustfreytag.com,www.augustfreytag.com --webroot /usr/share/nginx/html/challenges"
-docker-compose exec certbot /bin/bash -c "openssl dhparam -out /var/lib/letsencrypt/dhparam-2048.pem"
+docker-compose up -d
+docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_LETSENCRYPT:/etc/letsencrypt certbot/certbot certonly --agree-tos --email night@apricummedia.com -d augustfreytag.com --webroot -w /usr/share/nginx/html/challenges
+docker run -v freytag_data-letsencrypt:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
 docker-compose down
