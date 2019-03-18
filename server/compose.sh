9f8fb726c43f64e1d004e89af398e9cc8a4ce08d
August S. Freytag <night@apricummedia.com>
2019-03-18 18:20:57 +0100

    Move docker compose shell script to "server" subdirectory

--- server/compose.sh
+++ server/compose.sh
@@ -1,9 +1,7 @@
 if [ "$APP_ENVIRONMENT" = "LIVE" ]
 then
-	echo "Loading environment variables for LIVE."
 	source ./server/cycle-live.env
 else
-	echo "Loading environment variables for DEV."
 	source ./server/cycle-dev.env
 fi
 
