eec643ffc088bc0b8e4bc4d2b64602c33b895454
August S. Freytag <night@apricummedia.com>
2019-03-18 12:50:16 +0100

    Rewrite certbot provisioning and path for letsencrypt directory

--- server/provision-live.sh
+++ server/provision-live.sh
@@ -1,7 +1,7 @@
 source ./cycle-live.env
 export NGINX_CONF_NAME="nginx.dev.conf"
 
-docker-compose up proxy -d
-docker-compose exec certbot /bin/bash -c "certbot renew"
+docker-compose up -d proxy
+docker-compose exec certbot /bin/bash -c "run -d augustfreytag.com,www.augustfreytag.com --webroot /usr/share/nginx/html/challenges"
 docker-compose exec certbot /bin/bash -c "openssl dhparam -out /var/lib/letsencrypt/dhparam-2048.pem"
 docker-compose down
