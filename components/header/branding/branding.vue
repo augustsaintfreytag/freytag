d3fc52183b222faf1701f26bcfc5ff6729b4aaae
August S. Freytag <night@apricummedia.com>
2019-03-19 21:23:49 +0100

    Split header into subcomponents for branding and navigation

---
+++ components/header/branding/branding.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="branding">
+		<h1>{{ content.headerPrimaryTitle }}</h1>
+		<section class="tagline">{{ content.headerPrimaryOccupations }}</section>
+	</div>
+</template>
+
+<style lang="scss" src="./branding.scss"></style>
+
+<script lang="ts" src="./branding.ts"></script>
--- components/header/header.vue
+++ components/header/header.vue
@@ -1,36 +1,10 @@
 <template>
 	<header class="inset">
-		<div class="branding">
-			<h1>{{ content.headerPrimaryTitle }}</h1>
-			<section class="tagline">{{ content.headerPrimaryOccupations }}</section>
-		</div>
-		<nav>
-			<nuxt-link tag="div" v-for="route in [
-				{address: '/', name: 'Home'},
-				{address: '/life', name: 'Life'},
-				{address: '/work', name: 'Work'},
-				{address: '/hello', name: 'Contact'},
-			]" :key="route.address" v-bind:to="route.address">
-				<a>{{ route.name }}</a>
-			</nuxt-link>
-		</nav>
+		<header-branding-component />
+		<header-navigation-component />
 	</header>
 </template>
 
 <style lang="scss" src="./header.scss"></style>
 
-<script lang="ts">
-	import { Component, Vue } from "vue-property-decorator"
-
-	@Component
-	export default class HeaderComponent extends Vue {
-		data() {
-			return {
-				content: {
-					headerPrimaryTitle: "August S. Freytag",
-					headerPrimaryOccupations: ["Music Video Artist", "Editor", "Concept Designer", "Developer", "Analogue Photographer"].join(" ・ ")
-				}
-			}
-		}
-	}
-</script>
+<script lang="ts" src="./header.ts"></script>
