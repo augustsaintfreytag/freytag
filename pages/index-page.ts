06c7276f0189479b3acf04b6db8cfb2720a5f173
August S. Freytag <night@apricummedia.com>
2019-03-20 11:37:34 +0100

    Introduce mapper and landing page data interface for index

--- pages/index-page.ts
+++ pages/index-page.ts
@@ -1,12 +1,41 @@
 import { Component, Vue } from "vue-property-decorator"
 import HeaderComponent from "~/components/header/header.vue"
 import HeaderBrandingComponent from "~/components/header/branding/branding.vue"
+import { CockpitDataProvider } from "~/components/common/cockpit/providers/cockpit-data-provider"
+import { IndexData } from "./index-data"
+
+const data: IndexData = {
+	graphic: undefined
+}
+
+namespace IndexPageMapper {
+
+	export async function mapLandingGraphic(data: IndexData) {
+		try {
+			const landingGraphic = await CockpitDataProvider.landingGraphic()
+			data.graphic = landingGraphic
+		} catch (error) {
+			console.error("Could not get landing graphic.", error)
+		}
+	}
+
+}
 
 @Component({
 	layout: "landing",
+
 	components: {
 		HeaderComponent,
 		HeaderBrandingComponent
+	},
+
+	data() {
+		return data
+	},
+
+	async asyncData() {
+		await IndexPageMapper.mapLandingGraphic(data)
+		return data
 	}
 })
 export default class IndexPage extends Vue {}
