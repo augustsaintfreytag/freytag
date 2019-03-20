093bc06de6e3be3fc5d0151e2a34a83e497d2cf1
August S. Freytag <night@apricummedia.com>
2019-03-20 17:00:14 +0100

    Add catch-all fallback route definition for pages not found

---
+++ pages/*.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator"
+	import { Context } from "@nuxt/vue-app-edge"
+
+	@Component({
+		layout: "maintenance",
+
+		async asyncData(context: Context) {
+			return context.redirect(404, "/404")
+		}
+	})
+	export default class ErrorPage extends Vue {}
+</script>
