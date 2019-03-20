07ba25777a47999bec9250f6ecf739ff4c764200
August S. Freytag <night@apricummedia.com>
2019-03-20 13:10:26 +0100

    Update template and styling for index page and work showcase output

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,27 @@
 <template>
 	<section class="index-page">
-		<section class="introducer">
+		<section class="introducer" :style="`background-image: url(${ $root.managedResourcePath(graphic.asset.path) })`">
 			<header-branding-component />
 		</section>
-		<!--<section class="showcase">
-			<div class="title">A Long Covering Title</div>
-			<div class="image-holder"></div>
-		</section>-->
+		<section class="showcases inset" v-if="works && works.items.length">
+			<div class="leader">Selected Showcase</div>
+			<section class="work-showcase kind-blip-colored kind-all" v-for="workItem in works.items" :key="workItem.meta.id">
+				<nuxt-link :to="`/work/${ workItem.meta.id }`">
+					<div class="image-holder">
+						<img class="covering" v-if="workItem.titleImage" :src="$root.managedResourcePath(workItem.titleImage.path)" />
+					</div>
+					<div class="item-data">
+						<div class="title">{{ workItem.name }}</div>
+						<div class="blip"></div>
+						<div class="format">{{ workItem.event && workItem.event.format ? workItem.event.format : "Publication" }}</div>
+						<div class="contents">{{ workItem.numberOfSamples === 1 ? "1 sample" : `${workItem.numberOfSamples} samples` }}</div>
+					</div>
+				</nuxt-link>
+			</section>
+		</section>
+		<section class="spacer" style="height: 500px;">
+
+		</section>
 	</section>
 </template>
 
