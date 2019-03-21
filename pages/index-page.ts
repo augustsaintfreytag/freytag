60066a09d6b9c890bb17f98503c3471765a8923e
August S. Freytag <night@apricummedia.com>
2019-03-21 14:26:33 +0100

    Move index page mapper module to dedicated file

--- pages/index-page.ts
+++ pages/index-page.ts
@@ -1,34 +1,12 @@
 import { Component, Vue } from "vue-property-decorator"
 import HeaderComponent from "~/components/header/header.vue"
 import HeaderBrandingComponent from "~/components/header/branding/branding.vue"
-import { CockpitDataProvider } from "~/components/common/cockpit/providers/cockpit-data-provider"
 import { IndexData } from "./index-data"
+import { IndexPageMapper } from "./index-page-mapper"
 
 const data: IndexData = {
 	graphic: undefined,
 	works: undefined
-}
-
-namespace IndexPageMapper {
-
-	export async function mapLandingGraphic(data: IndexData) {
-		try {
-			const landingGraphic = await CockpitDataProvider.landingGraphic()
-			data.graphic = landingGraphic
-		} catch (error) {
-			console.error("Could not get landing graphic.", error)
-		}
-	}
-
-	export async function mapLandingWorks(data: IndexData) {
-		try {
-			const landingWorks = await CockpitDataProvider.landingWorks()
-			data.works = landingWorks
-		} catch (error) {
-			console.error("Could not get landing works.", error)
-		}
-	}
-
 }
 
 @Component({
