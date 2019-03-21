8f5292fa9a1e992d3f0a8ce4d5c6651a017f9d45
August S. Freytag <night@apricummedia.com>
2019-03-21 16:54:56 +0100

    Add separate cases for protocols and hostnames based on environment

--- components/common/configuration/cms-connection.ts
+++ components/common/configuration/cms-connection.ts
@@ -1,29 +1,50 @@
 import { Url } from "../library/url"
 import { Configuration } from "./configuration"
+import { Environment } from "./environment"
 
 export class CmsConnection implements Configuration.Connection {
+
+	private environment: Environment
+	private accessToken: Configuration.ApiToken|undefined
+
+	constructor() {
+		if (process.env["NUXT_APP_ENV"] === "LIVE") {
+			this.environment = Environment.Live
+		} else {
+			this.environment = Environment.Development
+		}
+
+		this.accessToken = process.env["NUXT_ENV_COCKPIT_ACCESS_TOKEN"]
+	}
 
 	// Parameters
 
 	protocol(): Configuration.Protocol {
+		if (this.environment === Environment.Live) {
+			return "https"
+		}
+
 		return "http"
 	}
 
 	host(context?: Configuration.Context): Url {
 		if (this.requestedContext(context) === Configuration.Context.Server) {
 			return "cockpit"
-		} else {
-			return "cockpit.intra"
 		}
+
+		if (this.environment === Environment.Live) {
+			return "cockpit.augustfreytag.com"
+		}
+
+		return "cockpit.intra"
 	}
 
 	token(): Configuration.ApiToken {
-		const token = process.env["NUXT_ENV_COCKPIT_ACCESS_TOKEN"]
-		if (!token) {
+		if (!this.accessToken) {
 			throw new TypeError(`Access token for cockpit access is not defined in environment.`)
 		}
 
-		return token
+		return this.accessToken
 
 	}
 
