84c3049e97b57e4af6b32887a4531c17e230ea46
August S. Freytag <night@apricummedia.com>
2019-03-21 10:58:46 +0100

    Establish footer structure for "me" and "map" listing and style rules

--- components/footer/footer.ts
+++ components/footer/footer.ts
@@ -1,6 +1,26 @@
 import { Component, Vue } from "vue-property-decorator"
+import { UrlComponent } from "../common/library/url"
 
-@Component({})
+interface MapRoute {
+	address: UrlComponent,
+	name: string
+}
+
+@Component({
+
+	computed: {
+		mapRoutes(): MapRoute[] {
+			return [
+				{address: "/", name: "Home"},
+				{address: "/life", name: "Life"},
+				{address: "/work", name: "Work"},
+				{address: "/impress", name: "Impress"},
+				{address: "/disclaimer", name: "Disclaimer"}
+			]
+		}
+	}
+
+})
 export default class FooterComponent extends Vue {
 
 }
--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,9 +1,39 @@
 <template>
 	<footer>
+		<div id="guide"></div>
+		<div class="wrap inset">
+			<div class="inlay">
+				<section class="personal">
+					<header>Me</header>
+					<main>
+						<div>me@augustfreytag.com</div>
+						<div>Columbiadamm<br />Berlin<br />Germany</div>
+					</main>
+				</section>
+				<section class="map">
+					<header>Map</header>
+					<main>
+						<nuxt-link v-for="route in mapRoutes" :key="route.address" :to="route.address">
+							<div class="route">
+								<div class="name">{{ route.name }}</div>
+								<div class="address">{{ route.address }}</div>
+							</div>
+						</nuxt-link>
+					</main>
+				</section>
+				<section class="public">
+					<header>Public</header>
+					<main></main>
+				</section>
+				<section class="further">
+					<header>Further</header>
+					<main></main>
+				</section>
+			</div>
+		</div>
 	</footer>
 </template>
 
 <style lang="scss" src="./footer.scss"></style>
 
 <script lang="ts" src="./footer.ts"></script>
-
