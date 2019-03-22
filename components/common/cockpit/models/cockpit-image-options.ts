c854aac9fb25bcb86646992dfdcc02baa554435f
August S. Freytag <night@apricummedia.com>
2019-03-22 15:04:12 +0100

    Introduce model definition and library content for cockpit image opts.

---
+++ components/common/cockpit/models/cockpit-image-options.ts
@@ -0,0 +1,97 @@
+import { Url } from "../../library/url"
+
+export namespace CockpitImage {
+
+	// Options
+
+	export class ImageOptions {
+
+		source: Url
+
+		mode: ImageMode = ImageMode.BestFit
+		filters: Filter[] = []
+		
+		width: number|undefined
+		height: number|undefined
+		quality: number = 0.75
+
+		includesDomainPath: boolean = false
+		isBase64Encoded: boolean = false
+
+		constructor({source, mode, filters, width, height, quality, includesDomainPath, isBase64Encoded}: {
+			source: Url
+			mode?: ImageMode
+			filters?: Filter[]
+			width?: number
+			height?: number
+			quality?: number
+			includesDomainPath?: boolean
+			isBase64Encoded?: boolean
+		}) {
+			this.source = source
+
+			if (mode) {
+				this.mode = mode
+			}
+
+			if (filters) {
+				this.filters = filters
+			}
+			
+			if (width) {
+				this.width = width
+			}
+
+			if (height) {
+				this.height = height
+			}
+			
+			if (quality) {
+				this.quality = quality
+			}
+			
+			this.includesDomainPath = includesDomainPath || false
+			this.isBase64Encoded = isBase64Encoded || false
+		}
+
+		// Formed Options
+
+		get options(): CockpitImageOptions {
+			return {
+				src: this.source,
+				m: this.mode,
+				f: this.filters,
+				w: this.width,
+				h: this.height,
+				q: this.quality,
+				d: this.includesDomainPath,
+				b64: this.isBase64Encoded
+			}
+		}
+
+	}
+
+	// Library
+
+	export type Filter = string
+
+	export enum ImageMode {
+		Thumbnail = "thumbnail",
+		BestFit = "bestFit",
+		Resize = "resize",
+		FitToWidth = "fitToWidth",
+		FitToHeight = "fitToHeight"
+	}
+
+	export interface CockpitImageOptions {
+		src?: Url,
+		m?: string,
+		f?: string[],
+		w?: number,
+		h?: number,
+		q?: number,
+		d?: boolean,
+		b64?: boolean
+	}
+
+}
