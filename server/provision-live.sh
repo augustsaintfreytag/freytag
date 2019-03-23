065556fd22ca06be1bd76f0371ff5facae77a2a5
August S. Freytag <night@apricummedia.com>
2019-03-23 16:58:25 +0100

    Add removal of ssl certificates in directory when provisioning live

--- server/provision-live.sh
+++ server/provision-live.sh
@@ -2,6 +2,7 @@ source ./env/cycle-live.env
 source ./env/provision-live.env
 
 docker-compose up -d
+docker run --rm -v $DOCKER_VOLUME:/etc/letsencrypt alpine /bin/sh -c "rm -rf /etc/letsencrypt/live/*"
 docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --agree-tos --email night@apricummedia.com -d $DOMAIN_NAMES_SSL --webroot -w /usr/share/nginx/html/challenges
 docker run -v freytag_data-letsencrypt:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
 docker-compose down
