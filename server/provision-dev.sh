4c9d0803e899b7b51b1152c25a697a023e5daadf
August S. Freytag <night@apricummedia.com>
2019-03-23 11:04:27 +0100

    Establish generation and linking of local certs with domain and alias
    Link output of generation to live domain names to be used with nginx proxy
    configuration, allow unchanged configs for dev and live.

--- server/provision-dev.sh
+++ server/provision-dev.sh
@@ -9,6 +9,10 @@ OUT_ROOT_CA_PEM_NAME="rootCA.pem"
 OUT_ROOT_CA_KEY_PATH=$OUT_DIRECTORY_BASE/$OUT_ROOT_CA_KEY_NAME
 OUT_ROOT_CA_PEM_PATH=$OUT_DIRECTORY_BASE/$OUT_ROOT_CA_PEM_NAME
 
+#############################################################
+#                   ROOT CERTIFICATE                        #
+#############################################################
+
 if ! test -f "$OUT_ROOT_CA_KEY_PATH"
 then
 	echo "Root key does not exist and will be generated."
@@ -22,20 +26,28 @@ else
 	echo "Root key already exists, will not be generated."
 fi
 
-for DOMAIN_NAME in $(echo $DOMAIN_NAMES_SSL | sed "s/,/ /g")
-do
+#############################################################
+#                   DOMAIN CERTIFICATES                     #
+#############################################################
+
+ORIGINAL_IFS=$IFS
+IFS=","
+
+echo "$DOMAIN_NAMES_SSL" | while read -d';' i j; do
+	DOMAIN_NAME=$i
+	DOMAIN_NAME_ALIAS=$j
+
 	OUT_DIRECTORY=$OUT_DIRECTORY_BASE/$DOMAIN_NAME
-	OUT_KEY_PATH=$OUT_DIRECTORY/out.key
-	OUT_PEM_PATH=$OUT_DIRECTORY/out.pem
+	OUT_KEY_PATH=$OUT_DIRECTORY/privkey.pem
 	OUT_CSR_PATH=$OUT_DIRECTORY/out.csr
-	OUT_CRT_PATH=$OUT_DIRECTORY/out.crt
+	OUT_CRT_PATH=$OUT_DIRECTORY/fullchain.pem
 
 	DOMAIN_CONFIG_PATH=./ssl-server-$DOMAIN_NAME.cnf
 	DOMAIN_V3_PATH=./ssl-server-v3-$DOMAIN_NAME.ext
 
 	if ! test -f "$OUT_CRT_PATH"
 	then
-		echo "Generating local certificate for domain '$DOMAIN_NAME'."
+		echo "Generating local certificate for domain '$DOMAIN_NAME' with alias '$DOMAIN_NAME_ALIAS'."
 
 		mkdir $OUT_DIRECTORY
 		
@@ -44,11 +56,21 @@ do
 
 		# Generate Certificate
 		openssl x509 -req -in $OUT_CSR_PATH -CA $OUT_ROOT_CA_PEM_PATH -CAkey $OUT_ROOT_CA_KEY_PATH -CAcreateserial -out $OUT_CRT_PATH -days 500 -sha256 -passin pass:$PLACEHOLDER_PASSWORD -extfile $DOMAIN_V3_PATH
+
+		# Remove Certificates
+		# docker run --rm -v $DOCKER_VOLUME:/etc/letsencrypt alpine /bin/sh -c "rm -rf /etc/letsencrypt/live/$DOMAIN_NAME_ALIAS"
+
+		# Copy Certificates
+		echo "Copying ssl contents from '$PWD' to volume named '$DOCKER_VOLUME'."
+		docker run --rm -v "$PWD":/source -v $DOCKER_VOLUME:/etc/letsencrypt alpine /bin/sh -c "mkdir -p /etc/letsencrypt/live/ && cp -R /source/ssl/* /etc/letsencrypt/live/"
+
+		echo "Setting up alias in ssl volume '$DOCKER_VOLUME' for domain '$DOMAIN_NAME' as link named '$DOMAIN_NAME_ALIAS'."
+		docker run --rm -v $DOCKER_VOLUME:/etc/letsencrypt alpine /bin/sh -c "ln -s /etc/letsencrypt/live/$DOMAIN_NAME /etc/letsencrypt/live/$DOMAIN_NAME_ALIAS"
 	else
-		echo "Certificate for domain '$DOMAIN_NAME' already exists."
+		echo "Certificate for domain '$DOMAIN_NAME' (alias '$DOMAIN_NAME_ALIAS') already exists."
 	fi
 done
 
-echo "Copying ssl contents from '$PWD' to volume named '$DOCKER_VOLUME'."
-docker run --rm -v "$PWD":/source -v $DOCKER_VOLUME:/etc/letsencrypt alpine /bin/sh -c "mkdir -p /etc/letsencrypt/live/ && cp -R /source/ssl/* /etc/letsencrypt/live/"
+IFS=$ORIGINAL_IFS
+
 docker run -v $DOCKER_VOLUME:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
