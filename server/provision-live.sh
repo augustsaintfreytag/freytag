037c1123d09bde2fcb50f584fa4f62b8b6f22d48
August S. Freytag <night@apricummedia.com>
2019-03-24 17:49:14 +0100

    Fix live provi. script not using correct parameter label for domains

--- server/provision-live.sh
+++ server/provision-live.sh
@@ -16,7 +16,7 @@ docker run --rm -v $VOLUME_SSL:/etc/letsencrypt alpine /bin/sh -c "rm -rf /etc/l
 for DOMAIN_NAME in $(echo $DOMAIN_NAME_PARAMS | sed "s/,/ /g")
 do
     echo "Creating new certificate for domain '$DOMAIN_NAME'."
-	docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --agree-tos --email night@apricummedia.com $DOMAIN_NAME --webroot -w /usr/share/nginx/html/challenges
+	docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --agree-tos --email night@apricummedia.com -d $DOMAIN_NAME --webroot -w /usr/share/nginx/html/challenges
 done
 
 docker run -v $VOLUME_SSL:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
