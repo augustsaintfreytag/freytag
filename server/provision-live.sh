3d1ac7394140ee02e764aa2f8475d07a61d0897f
August S. Freytag <night@apricummedia.com>
2019-03-24 17:47:02 +0100

    Extend live provision script to create certificates for *all* domains
    Previous script onyl created single domain and certbot ignored others.

--- server/provision-live.sh
+++ server/provision-live.sh
@@ -12,6 +12,12 @@ source "$BASE/env/provision-live.env"
 
 docker-compose up -d
 docker run --rm -v $VOLUME_SSL:/etc/letsencrypt alpine /bin/sh -c "rm -rf /etc/letsencrypt/live && mv /etc/letsencrypt/live-displaced /etc/letsencrypt/live"
-docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --agree-tos --email night@apricummedia.com $DOMAIN_NAME_PARAMS --webroot -w /usr/share/nginx/html/challenges
-docker run -v freytag_data-letsencrypt:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
+
+for DOMAIN_NAME in $(echo $DOMAIN_NAME_PARAMS | sed "s/,/ /g")
+do
+    echo "Creating new certificate for domain '$DOMAIN_NAME'."
+	docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --agree-tos --email night@apricummedia.com $DOMAIN_NAME --webroot -w /usr/share/nginx/html/challenges
+done
+
+docker run -v $VOLUME_SSL:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
 docker-compose down
