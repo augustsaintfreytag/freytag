67e4166018a1ebc065af9b9971d03271ea899af2
August S. Freytag <night@apricummedia.com>
2019-03-26 15:54:07 +0100

    Introduce row names for life event detail table as filter options
    Merge definitions namespace back into component.

--- app/components/life-event/life-event-detail-table.ts
+++ app/components/life-event/life-event-detail-table.ts
@@ -1,17 +1,50 @@
 import { Component, Vue } from "vue-property-decorator"
-import { LifeEventDefinitions } from "./life-event-definitions"
+import { Vita } from "../common/storage/models/vita-event"
+import { DateFormatter } from "../common/storage/providers/date-formatter"
+
+interface DetailRowDefinition {
+	name: string
+	value: string|undefined
+}
+
+function detailRows(lifeEvent: Vita.Event): DetailRowDefinition[] {
+	return [
+		{name: "Title", value: lifeEvent.name},
+		{name: "Span", value: DateFormatter.formattedDateRange(lifeEvent)},
+		{name: "Kind", value: lifeEvent.kind},
+		{name: "Format", value: lifeEvent.format},
+		{name: "Role", value: lifeEvent.role},
+		{name: "Context", value: lifeEvent.context},
+		{name: "Description", value: lifeEvent.description}
+	]
+}
 
 @Component({
-	props: ["lifeEvent"],
+	props: ["lifeEvent", "rowNames"],
 
 	computed: {
 		detailRows() {
 			const event = this.$props.lifeEvent
+			const rowNames = this.$props.rowNames as string[]
+			const rows: DetailRowDefinition[] = []
+			
 			if (!event) {
-				return []
+				return rows
 			}
 
-			return LifeEventDefinitions.detailRows(event)
+			if (!rowNames) {
+				return detailRows(event)
+			}
+
+			for (const row of detailRows(event)) {
+				if (!rowNames.includes(row.name)) {
+					continue
+				}
+
+				rows.push(row)
+			}
+
+			return rows
 		}
 	}
 })
