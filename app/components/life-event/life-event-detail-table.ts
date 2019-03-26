2d929d7224bf3f05e3c452914853fd858cb6653c
August S. Freytag <night@apricummedia.com>
2019-03-26 22:36:38 +0100

    Restructure detail table logic into namespace, add empty row filtering
    Arrange computed property for detail rows, rename interface for rows.

--- app/components/life-event/life-event-detail-table.ts
+++ app/components/life-event/life-event-detail-table.ts
@@ -2,47 +2,60 @@ import { Component, Vue } from "vue-property-decorator"
 import { Vita } from "../common/storage/models/vita-event"
 import { DateFormatter } from "../common/storage/providers/date-formatter"
 
-interface DetailRowDefinition {
+interface DetailRow {
 	name: string
 	value: string|undefined
 }
 
-function detailRows(lifeEvent: Vita.Event): DetailRowDefinition[] {
-	return [
-		{name: "Title", value: lifeEvent.name},
-		{name: "Span", value: DateFormatter.formattedDateRange(lifeEvent)},
-		{name: "Kind", value: lifeEvent.kind},
-		{name: "Format", value: lifeEvent.format},
-		{name: "Role", value: lifeEvent.role},
-		{name: "Location", value: lifeEvent.location},
-		{name: "Context", value: lifeEvent.context},
-		{name: "Description", value: lifeEvent.description}
-	]
+namespace LifeEventDetailTable {
+
+	export function filteredRowsByNames(rows: DetailRow[], rowNames: string[]): DetailRow[] {
+		return rows.filter(row => {
+			return rowNames.includes(row.name)
+		})
+	}
+
+	export function filteredRowsByValue(rows: DetailRow[]): DetailRow[] {
+		return rows.filter(row => {
+			return row.value !== undefined && row.value !== ""
+		})
+	}
+
+	export function rowsFromEvent(lifeEvent: Vita.Event): DetailRow[] {
+		return [
+			{name: "Title", value: lifeEvent.name},
+			{name: "Span", value: DateFormatter.formattedDateRange(lifeEvent)},
+			{name: "Kind", value: lifeEvent.kind},
+			{name: "Format", value: lifeEvent.format},
+			{name: "Role", value: lifeEvent.role},
+			{name: "Location", value: lifeEvent.location},
+			{name: "Context", value: lifeEvent.context},
+			{name: "Description", value: lifeEvent.description}
+		]
+	}
+
 }
 
 @Component({
-	props: ["lifeEvent", "rowNames"],
+	props: ["lifeEvent", "rowNames", "showsEmptyRows"],
 
 	computed: {
 		detailRows() {
 			const event = this.$props.lifeEvent
 			const rowNames = this.$props.rowNames as string[]
-			const rows: DetailRowDefinition[] = []
 			
 			if (!event) {
-				return rows
+				return []
 			}
-
-			if (!rowNames) {
-				return detailRows(event)
+			
+			let rows: DetailRow[] = LifeEventDetailTable.rowsFromEvent(event)
+			
+			if (!this.$props.showsEmptyRows) {
+				rows = LifeEventDetailTable.filteredRowsByValue(rows)
 			}
-
-			for (const row of detailRows(event)) {
-				if (!rowNames.includes(row.name)) {
-					continue
-				}
-
-				rows.push(row)
+			
+			if (rowNames) {
+				rows = LifeEventDetailTable.filteredRowsByNames(rows, rowNames)
 			}
 
 			return rows
