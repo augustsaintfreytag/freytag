f520f6feb1b5a16125288311067ec08c2a019139
August S. Freytag <night@apricummedia.com>
2019-03-27 15:19:23 +0100

    Update transcribed types for meta data block in cockpit models
    Set "created" and "creator" as non-optionals, update writing

--- app/components/common/storage/models/meta-data.ts
+++ app/components/common/storage/models/meta-data.ts
@@ -6,19 +6,21 @@ export default class MetaData {
 	
 	id: UUID
 	
-	creator: UUID|undefined
+	creator: UUID
 	editor: UUID|undefined
 
-	created: Date|undefined
+	created: Date
 	modified: Date|undefined
 
 	constructor(entry: CockpitEntry) {
 		this.id = entry._id
 		this.creator = entry._by
-		this.editor = entry._mby
+		this.editor = entry._mby || undefined
 
 		if (entry._created) {
-			this.created = ConversionProvider.dateFromTimestamp(entry._created)
+			this.created = ConversionProvider.dateFromTimestamp(entry._created)!
+		} else {
+			this.created = new Date()
 		}
 
 		if (entry._modified) {
