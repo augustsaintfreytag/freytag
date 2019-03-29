12cefafad8f4c3c9faf1d5657a514ecc006ecd1d
August S. Freytag <night@apricummedia.com>
2019-03-29 10:23:17 +0100

    Remove now obsolete computed property for paragr. splitting in text col.

--- app/components/content-blocks/text-column-block/text-column-block.ts
+++ app/components/content-blocks/text-column-block/text-column-block.ts
@@ -1,5 +1,4 @@
 import { Component, Vue } from "vue-property-decorator"
-import { Content } from "~/components/common/storage/models/content-block"
 import Markdown from "~/components/markdown/markdown.vue"
 
 @Component({
@@ -7,16 +6,6 @@ import Markdown from "~/components/markdown/markdown.vue"
 		Markdown
 	},
 
-	computed: {
-		textParagraphs(): string[] {
-			const textContent = (this.$props.contentBlock as Content.TextColumnBlock).textContent
-			if (!textContent) {
-				return []
-			}
-
-			return textContent.replace(/\n+/, "\n").split("\n")
-		}
-	}
 	props: ["contentBlock"]
 })
 export default class TextColumnBlockComponent extends Vue {}
