f738aa8d289144d044fe8eb8f0986e44ce900073
August S. Freytag <night@apricummedia.com>
2019-03-29 10:21:32 +0100

    Introduce reusable, preconfigured markdown component using "vue-md"

---
+++ app/components/markdown/markdown.vue
@@ -0,0 +1,7 @@
+<template>
+	<vue-markdown :anchor-attributes="{ target: '_blank', rel: 'noopener' }">
+		<slot></slot>
+	</vue-markdown>
+</template>
+
+<script lang="ts" src="./markdown.ts"></script>
