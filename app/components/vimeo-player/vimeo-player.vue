6e25aea1e278b38a3d496f4a36504e4b9036d7fe
August S. Freytag <night@apricummedia.com>
2019-04-10 14:01:27 +0200

    Displace vimeo comp. styling to sheet, add aspect value prop and use

--- app/components/vimeo-player/vimeo-player.vue
+++ app/components/vimeo-player/vimeo-player.vue
@@ -1,8 +1,11 @@
 <template>
-	<div class="vimeo-player" style="padding:75% 0 0 0;position:relative;">
-		<iframe :src="`https://player.vimeo.com/video/${videoCode}?color=ffffff&byline=0&portrait=0`" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	<div class="vimeo-player" :style="{ 'padding-top': videoAspectValue || '75%' }">
+		<iframe :src="`https://player.vimeo.com/video/${videoCode}?color=ffffff&byline=0&portrait=0`" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 		<script src="https://player.vimeo.com/api/player.js"></script>
 	</div>
 </template>
+
+<style lang="scss" src="./vimeo-player.scss"></style>
+
 
 <script lang="ts" src="./vimeo-player.ts"></script>
