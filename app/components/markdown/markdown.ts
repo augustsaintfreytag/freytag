924bad1e06ab9d59d9ba4065d2dc46cc743fb18d
August S. Freytag <night@apricummedia.com>
2019-04-12 21:45:41 +0200

    Introduce custom link anchor supplements for target and rel attribs.
    Add "noopener" and target "_blank" for all links.

--- app/components/markdown/markdown.ts
+++ app/components/markdown/markdown.ts
@@ -1,7 +1,21 @@
 import { Component, Vue } from "vue-property-decorator"
 import Markdown from "markdown-it"
 
-const markdown = Markdown()
+namespace Modules {
+
+	export const markdown = Markdown({
+		linkify: true
+	})
+
+	export function supplemented(input: string): string {
+		return withSupplementedAnchors(input)
+	}
+
+	export function withSupplementedAnchors(input: string): string {
+		return input.replace(/(<a\s*href=".+?")(\s*>)/g, "$1 target=\"_blank\" rel=\"noopener\"$2")
+	}
+
+}
 
 @Component({
 	props: [
@@ -10,9 +24,9 @@ const markdown = Markdown()
 
 	data() {
 		const sourceString = this.$props["content"] as string
-		const renderedString = markdown.render(sourceString)
+		const renderedString = Modules.markdown.render(sourceString)
 
-		return { output: renderedString }
+		return { output: Modules.supplemented(renderedString) }
 	}
 })
 export default class MarkdownComponent extends Vue {}
