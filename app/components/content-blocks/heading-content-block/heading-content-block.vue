1a300ce2f37279dbe927eff797caa5b3ec8d0c8d
August S. Freytag <night@apricummedia.com>
2019-05-10 11:32:28 +0200

    Introduce vue component for heading content block

---
+++ app/components/content-blocks/heading-content-block/heading-content-block.vue
@@ -0,0 +1,10 @@
+<template>
+	<section class="content-block heading-content-block text inset" v-if="contentBlock">
+		<h2>{{ contentBlock.textContent }}</h2>
+	</section>
+</template>
+
+<style lang="scss" src="./heading-content-block.scss"></style>
+
+<script lang="ts" src="./heading-content-block.ts"></script>
+
