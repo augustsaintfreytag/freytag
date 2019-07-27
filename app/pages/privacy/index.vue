affccadcfff707e2cfa8bae17b316e72ab9b8f58
August S. Freytag <night@apricummedia.com>
2019-07-27 14:54:44 +0200

    Establish privacy policy page with formatted German GDPR discl. text

---
+++ app/pages/privacy/index.vue
@@ -0,0 +1,84 @@
+<template>
+	<section class="privacy-policy-page inset text">
+		<article>
+			<h2>
+				<div>Datenschutzerklärung</div>
+				<div class="subheading">Nach Regulation (EU) 2016/679 (General Data Protection Regulation)</div>
+			</h2>
+			<div>
+				<p>Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der EU-Datenschutzgrundverordnung (DSGVO), ist:</p>
+				<contact-block-component />
+			</div>
+			<div>
+				<h3>Ihre Betroffenenrechte</h3>
+				<p>Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten können Sie jederzeit folgende Rechte ausüben:</p>
+				<ul>
+					<li>Auskunft über Ihre bei uns gespeicherten Daten und deren Verarbeitung (Art. 15 DSGVO),</li>
+					<li>Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),</li>
+					<li>Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),</li>
+					<li>Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),</li>
+					<li>Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO) und</li>
+					<li>Datenübertragbarkeit, sofern Sie in die Datenverarbeitung eingewilligt haben oder einen Vertrag mit uns abgeschlossen haben (Art. 20 DSGVO).</li>
+				</ul>
+				<p>Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese jederzeit mit Wirkung für die Zukunft widerrufen. Sie können sich jederzeit mit einer Beschwerde an eine Aufsichtsbehörde wenden, z. B. an die zuständige Aufsichtsbehörde des Bundeslands Ihres Wohnsitzes oder an die für uns als verantwortliche Stelle zuständige Behörde. Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen Bereich) mit Anschrift finden Sie beim <a href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html" target="_blank" rel="nofollow noopener">BfDI</a>.</p>
+				<div>
+					<h4>Art und Zweck der Verarbeitung</h4>
+					<p>Wenn Sie auf unsere Website zugreifen, d.h., wenn Sie sich nicht registrieren oder anderweitig Informationen übermitteln, werden automatisch Informationen allgemeiner Natur erfasst. Diese Informationen (Server-Logfiles) beinhalten etwa die Art des Webbrowsers, das verwendete Betriebssystem, den Domainnamen Ihres Internet-Service-Providers, Ihre IP-Adresse und ähnliches.</p>
+					<p>Sie werden insbesondere zu folgenden Zwecken verarbeitet:</p>
+					<ul>
+						<li>Sicherstellung eines problemlosen Verbindungsaufbaus der Website,</li>
+						<li>Sicherstellung einer reibungslosen Nutzung unserer Website,</li>
+						<li>Auswertung der Systemsicherheit und -stabilität sowie</li>
+						<li>zu weiteren administrativen Zwecken.</li>
+					</ul>
+					<p>Wir verwenden Ihre Daten nicht, um Rückschlüsse auf Ihre Person zu ziehen. Informationen dieser Art werden von uns ggfs. statistisch ausgewertet, um unseren Internetauftritt und die dahinterstehende Technik zu optimieren.</p>
+				</div>
+				<div>
+					<h4>Rechtsgrundlage</h4>
+					<p>Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. f DSGVO auf Basis unseres berechtigten Interesses an der Verbesserung der Stabilität und Funktionalität unserer Website.</p>
+				</div>
+				<div>
+					<h4>Empfänger</h4>
+					<p>Empfänger der Daten sind ggf. technische Dienstleister, die für den Betrieb und die Wartung unserer Webseite als Auftragsverarbeiter tätig werden.</p>
+				</div>
+				<div>
+					<h4>Speicherdauer</h4>
+					<p>Die Daten werden gelöscht, sobald diese für den Zweck der Erhebung nicht mehr erforderlich sind. Dies ist für die Daten, die der Bereitstellung der Webseite dienen, grundsätzlich der Fall, wenn die jeweilige Sitzung beendet ist.</p>
+				</div>
+				<div>
+					<h4>Bereitstellung vorgeschrieben oder erforderlich</h4>
+					<p>Die Bereitstellung der vorgenannten personenbezogenen Daten ist weder gesetzlich noch vertraglich vorgeschrieben. Ohne die IP-Adresse ist jedoch der Dienst und die Funktionsfähigkeit unserer Website nicht gewährleistet. Zudem können einzelne Dienste und Services nicht verfügbar oder eingeschränkt sein. Aus diesem Grund ist ein Widerspruch ausgeschlossen. </p>
+				</div>
+			</div>
+			<div>
+				<h3>SSL-Verschlüsselung</h3>
+				<p>Um die Sicherheit Ihrer Daten bei der Übertragung zu schützen, verwenden wir dem aktuellen Stand der Technik entsprechende Verschlüsselungsverfahren (z. B. SSL) über HTTPS.</p>
+			</div>
+			<div>
+				<h3>Änderung unserer Datenschutzbestimmungen</h3>
+				<p>Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie stets den aktuellen rechtlichen Anforderungen entspricht oder um Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen, z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt dann die neue Datenschutzerklärung.</p>
+			</div>
+			<div>
+				<h3>Fragen an den Datenschutzbeauftragten</h3>
+				<p>Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte eine E-Mail oder wenden Sie sich direkt an den Inhaber und Betreiber der Seite.</p>
+			</div>
+			<div>
+				<p>Die Datenschutzerklärung wurde mithilfe der activeMind AG erstellt, den Experten für <a href="https://www.activemind.de/datenschutz/datenschutzhinweis-generator/" target="_blank" rel="noopener">externe Datenschutzbeauftragte</a> (Version #2019-04-10).</p>
+			</div>
+		</article>
+	</section>
+</template>
+
+<style lang="scss" src="./index.scss"></style>
+
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator"
+	import ContactBlockComponent from "~/components/contact-block/contact-block.vue"
+
+	@Component({
+		components: { ContactBlockComponent }
+	})
+	export default class PrivacyPolicyPage extends Vue {}
+
+</script>
