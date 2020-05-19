fb3f16c90834f46b65f1c327ce012b783c455422
August S. Freytag <night@apricummedia.com>
2020-05-19 11:34:31 +0200

    Supplement missing port passthrough in live standalone script

--- server/provision-live-standalone.sh
+++ server/provision-live-standalone.sh
@@ -9,7 +9,7 @@ docker-compose stop proxy
 for DOMAIN_NAME in $(echo $DOMAIN_NAME_PARAMS | sed "s/,/ /g")
 do
     echo "Creating new certificate for domain '$DOMAIN_NAME'."
-	docker run -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --standalone -preferred-challenges http --agree-tos --email night@apricummedia.com -d $DOMAIN_NAME
+	docker run -p 80:80 -v $VOLUME_CHALLENGES:/usr/share/nginx/html/challenges -v $VOLUME_SSL:/etc/letsencrypt certbot/certbot certonly --standalone --preferred-challenges http --agree-tos --email night@apricummedia.com -d $DOMAIN_NAME
 done
 
 docker run -v $VOLUME_SSL:/etc/letsencrypt frapsoft/openssl dhparam -dsaparam -out /etc/letsencrypt/dhparam-2048.pem 2048
