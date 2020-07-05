32e52cb9a9f949e0a32d96a7544a79b28b152bba
August Saint Freytag <night@apricummedia.com>
2020-07-05 12:56:25 +0200

    Hotfix readout of title graphic when missing data

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,8 +4,8 @@
 			<div class="title-holder">
 				<header-branding-component />
 			</div>
-			<div class="caption" v-if="graphic.caption">{{ graphic.caption }}</div>
-			<div class="image-holder">
+			<div class="caption" v-if="graphic && graphic.caption">{{ graphic.caption }}</div>
+			<div class="image-holder" v-if="graphic">
 				<img class="covering" :src="$imagePath(graphic.asset.path, 'large')" />
 			</div>
 		</section>
