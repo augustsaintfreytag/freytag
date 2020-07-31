df49c66b0c60daf0088078fabbda7964a14ef145
August Saint Freytag <night@apricummedia.com>
2020-07-31 13:58:54 +0200

    Update index page component, update data interface and data return

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -31,8 +31,8 @@
 			</section>
 		</section>
 		<aside class="opt">
-			<p>Portfolio site of August Freytag (August Saint Freytag, August S. Freytag, A.S.F.), presenting a life overview and work showcases, publications, further platforms and projects and contact options.</p>
-			<p>All displayed contents are intellectual property of August Freytag (August Saint Freytag, August S. Freytag, A.S.F.), all rights reserved. Denoted contents belong to their respective owners.</p>
+			<p>Portfolio site of August Saint Freytag (named as August Freytag, August S. Freytag, A.S.F.), presenting a life overview and work showcases, publications, further platforms and projects and contact options.</p>
+			<p>All displayed contents are intellectual property of August Saint Freytag (named as August Freytag, August S. Freytag, A.S.F.), all rights reserved. Denoted contents belong to their respective owners.</p>
 		</aside>
 	</section>
 </template>
