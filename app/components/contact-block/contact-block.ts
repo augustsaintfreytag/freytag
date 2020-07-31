fc544c4bceaad0b2209036104fa77c3234d82b55
August Saint Freytag <night@apricummedia.com>
2020-07-31 10:13:22 +0200

    Update contact block component, add in-component data

--- app/components/contact-block/contact-block.ts
+++ app/components/contact-block/contact-block.ts
@@ -1,29 +1,38 @@
 import { Component, Vue } from "vue-property-decorator"
 import Rot13 from "../common/address-obfuscator/provider/rot-13"
 
+// Modules
+
 const rot13 = new Rot13()
 
-interface ContactBlockData {
+// Library
+
+interface Data {
 	phoneNumber: string
 	mailAddress: string
 }
 
+// Component
+
 @Component({
-	data() {
-		const initialData: ContactBlockData = {
+	data(): Data {
+		return {
 			phoneNumber: rot13.encoded("030 915 779 70"),
 			mailAddress: rot13.encoded("me@augustfreytag.com")
 		}
-
-		return initialData
 	},
 
 	created() {
 		if (process.browser) {
-			const data = this.$data as ContactBlockData
+			const data = this.$data as Data
 			data.phoneNumber = rot13.decoded(data.phoneNumber)
 			data.mailAddress = rot13.decoded(data.mailAddress)
 		}
 	}
 })
-export default class ContactBlockComponent extends Vue {}
+export default class ContactBlockComponent extends Vue implements Data {
+
+	phoneNumber!: string
+	mailAddress!: string
+
+}
