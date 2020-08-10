5949029ebd69f372ee72a26332c2758d99bd8a81
August Saint Freytag <night@apricummedia.com>
2020-08-10 16:49:24 +0200

    Reformat cockpit image request preset declaration file

--- app/utils/cockpit/library/cockpit-image-request-presets.ts
+++ app/utils/cockpit/library/cockpit-image-request-presets.ts
@@ -1,53 +1,49 @@
 import { CockpitImageOptions, CockpitImageRequest } from "cockpit-access"
 
-export namespace CockpitImageRequestPreset {
+const defaultQuality = 75
 
-	const defaultQuality = 75
+// Library
 
-	// Library
+export enum Format {
 
-	export enum Format {
+	Small = "small",
+	Regular = "regular",
+	Large = "large"
 
-		Small = "small",
-		Regular = "regular",
-		Large = "large"
+}
+
+type PresetsByFormat = {[key: string]: CockpitImageRequest}
+
+// Preset Preparation
+
+const presets: PresetsByFormat = {}
+
+presets[Format.Small] = new CockpitImageRequest({
+	mode: CockpitImageOptions.Mode.BestFit,
+	width: 600,
+	quality: defaultQuality
+})
+
+presets[Format.Regular] = new CockpitImageRequest({
+	mode: CockpitImageOptions.Mode.BestFit,
+	width: 1000,
+	quality: defaultQuality
+})
+
+presets[Format.Large] = new CockpitImageRequest({
+	mode: CockpitImageOptions.Mode.BestFit,
+	width: 1600,
+	quality: defaultQuality
+})
+
+// Preset Supply
+
+export function preset(format: Format): CockpitImageRequest {
+	const preset = presets[format]
 	
+	if (!preset) {
+		throw new TypeError(`Supplied format is not valid, can not return image request preset.`)
 	}
 
-	type PresetsByFormat = {[key: string]: CockpitImageRequest}
-
-	// Preset Preparation
-
-	const presets: PresetsByFormat = {}
-
-	presets[Format.Small] = new CockpitImageRequest({
-		mode: CockpitImageOptions.Mode.BestFit,
-		width: 600,
-		quality: defaultQuality
-	})
-
-	presets[Format.Regular] = new CockpitImageRequest({
-		mode: CockpitImageOptions.Mode.BestFit,
-		width: 1000,
-		quality: defaultQuality
-	})
-
-	presets[Format.Large] = new CockpitImageRequest({
-		mode: CockpitImageOptions.Mode.BestFit,
-		width: 1600,
-		quality: defaultQuality
-	})
-
-	// Preset Supply
-
-	export function preset(format: Format): CockpitImageRequest {
-		const preset = presets[format]
-		
-		if (!preset) {
-			throw new TypeError(`Supplied format is not valid, can not return image request preset.`)
-		}
-
-		return preset
-	}
-
+	return preset
 }
