c7f6e02e8ad02cea8801ec4b6acfd501420486b6
August Saint Freytag <night@apricummedia.com>
2020-08-12 17:00:52 +0200

    Refactor image request preset form with switch statement

--- app/utils/cockpit/library/cockpit-image-request-presets.ts
+++ app/utils/cockpit/library/cockpit-image-request-presets.ts
@@ -12,38 +12,29 @@ export enum Format {
 
 }
 
-type PresetsByFormat = {[key: string]: CockpitImageRequest}
-
-// Preset Preparation
-
-const presets: PresetsByFormat = {}
-
-presets[Format.Small] = new CockpitImageRequest({
-	mode: CockpitImageOptions.Mode.BestFit,
-	width: 600,
-	quality: defaultQuality
-})
-
-presets[Format.Regular] = new CockpitImageRequest({
-	mode: CockpitImageOptions.Mode.BestFit,
-	width: 1000,
-	quality: defaultQuality
-})
-
-presets[Format.Large] = new CockpitImageRequest({
-	mode: CockpitImageOptions.Mode.BestFit,
-	width: 1600,
-	quality: defaultQuality
-})
-
 // Preset Supply
 
 export function preset(format: Format): CockpitImageRequest {
-	const preset = presets[format]
-	
-	if (!preset) {
-		throw new TypeError(`Supplied format is not valid, can not return image request preset.`)
+	switch (format) {
+		case Format.Small:
+			return new CockpitImageRequest({
+				mode: CockpitImageOptions.Mode.BestFit,
+				width: 600,
+				quality: defaultQuality
+			})
+		case Format.Regular:
+			return new CockpitImageRequest({
+				mode: CockpitImageOptions.Mode.BestFit,
+				width: 1000,
+				quality: defaultQuality
+			})
+		case Format.Large:
+			return new CockpitImageRequest({
+				mode: CockpitImageOptions.Mode.BestFit,
+				width: 1600,
+				quality: defaultQuality
+			})
+		default:
+			throw new TypeError(`Unsupported format '${format}' for image request preset.`)
 	}
-
-	return preset
 }
