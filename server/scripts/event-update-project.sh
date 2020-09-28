5e2a4e23f54cd4d4f7c87a9c63bfe527e347aa89
August Saint Freytag <night@apricummedia.com>
2020-09-28 20:25:05 +0200

    Extend autonomous project update script, pull most recent version first
    Ansible playbooks may have been updated and are needed. Only works if the
    configured "app_version" did not change between the last and the new
    deployment; branches are only changed by Ansible using the old services
    playbook. Project update may have to run twice, once to update to the new
    branch, once to execute the new playbook.

--- server/scripts/event-update-project.sh
+++ server/scripts/event-update-project.sh
@@ -4,6 +4,11 @@
 # Run through entire services playbook, use existing host-provided parameters.
 
 BASEDIR=$(dirname "$0")
-cd "$BASEDIR/../ansible"
 
+# Reset and Update Project
+cd "$BASEDIR/../../"
+git reset --hard && git pull
+
+# Run Ansible Service Set-Up Playbook
+cd "$BASEDIR/../ansible"
 ansible-playbook ./playbook-set-up-services.yml --extra-vars="override_hosts=localhost" --skip-tags="requires_host, listener"
