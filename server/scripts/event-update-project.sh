6e9c7c4486e895d3c10a4111a3dfbb0c52a8919a
August Saint Freytag <night@apricummedia.com>
2020-09-28 20:26:13 +0200

    Remove local project update, require priming if needed

--- server/scripts/event-update-project.sh
+++ server/scripts/event-update-project.sh
@@ -5,10 +5,6 @@
 
 BASEDIR=$(dirname "$0")
 
-# Reset and Update Project
-cd "$BASEDIR/../../"
-git reset --hard && git pull
-
 # Run Ansible Service Set-Up Playbook
 cd "$BASEDIR/../ansible"
 ansible-playbook ./playbook-set-up-services.yml --extra-vars="override_hosts=localhost" --skip-tags="requires_host, listener"
