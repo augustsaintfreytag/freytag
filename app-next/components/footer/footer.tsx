6fc8f9344441e811e6603603b19a3c21510f5d6c
August Saint Freytag <night@apricummedia.com>
2021-03-10 20:57:27 +0100

    Establish inlay elements for footer and header, add "inlayed" mixin
    Merge mixins with existing sheets, update imports and usage. Adopt camel case
    for all mixin names.

--- app-next/components/footer/footer.tsx
+++ app-next/components/footer/footer.tsx
@@ -13,31 +13,33 @@ enum SpriteHref {
 
 const Footer: FunctionComponent = () => (
 	<footer className={styles.footer}>
-		<FooterSection header="Me">
-			<FooterItem sprite={SpriteHref.Broadcast} text="mailman@augustfreytag.com" />
-			<FooterItem
-				sprite={SpriteHref.Map}
-				text={
-					<>
-						The Heart of the Capital
-						<br />
-						Berlin, Germany
-					</>
-				}
-			/>
-		</FooterSection>
-		<FooterSection header="Map">
-			<FooterItem text="Home" link="/" />
-			<FooterItem text="Life" link="/life" />
-			<FooterItem text="Work" link="/work" />
-			<FooterItem text="Imprint" link="/imprint" />
-		</FooterSection>
-		<FooterSection header="Further">
-			<FooterItem sprite={SpriteHref.Vimeo} text="Vimeo" link="https://vimeo.com/apricum" />
-			<FooterItem sprite={SpriteHref.GitLab} text="GitLab" link="https://gitlab.com/apricum" />
-			<FooterItem sprite={SpriteHref.Twitter} text="Twitter" link="https://twitter.com/augustfreytag" />
-			<FooterItem sprite={SpriteHref.Medium} text="Medium" link="https://medium.com/@augustfreytag" />
-		</FooterSection>
+		<div className={styles.inlay}>
+			<FooterSection header="Me">
+				<FooterItem sprite={SpriteHref.Broadcast} text="mailman@augustfreytag.com" />
+				<FooterItem
+					sprite={SpriteHref.Map}
+					text={
+						<>
+							The Heart of the Capital
+							<br />
+							Berlin, Germany
+						</>
+					}
+				/>
+			</FooterSection>
+			<FooterSection header="Map">
+				<FooterItem text="Home" link="/" />
+				<FooterItem text="Life" link="/life" />
+				<FooterItem text="Work" link="/work" />
+				<FooterItem text="Imprint" link="/imprint" />
+			</FooterSection>
+			<FooterSection header="Further">
+				<FooterItem sprite={SpriteHref.Vimeo} text="Vimeo" link="https://vimeo.com/apricum" />
+				<FooterItem sprite={SpriteHref.GitLab} text="GitLab" link="https://gitlab.com/apricum" />
+				<FooterItem sprite={SpriteHref.Twitter} text="Twitter" link="https://twitter.com/augustfreytag" />
+				<FooterItem sprite={SpriteHref.Medium} text="Medium" link="https://medium.com/@augustfreytag" />
+			</FooterSection>
+		</div>
 	</footer>
 )
 
--- app-next/components/header/header.tsx
+++ app-next/components/header/header.tsx
@@ -14,18 +14,20 @@ const [titleText, descriptorText] = headerText()
 
 const Header: FunctionComponent = () => (
 	<header className={styles.header}>
-		<section className={styles.leader}>
-			<h1 className={styles.title}>{titleText}</h1>
-			<div className={styles.descriptors}>{descriptorText}</div>
-		</section>
-		<nav className={styles.trailer}>
-			<ol>
-				<li>Home</li>
-				<li>Life</li>
-				<li>Work</li>
-				<li>Imprint</li>
-			</ol>
-		</nav>
+		<div className={styles.inlay}>
+			<section className={styles.leader}>
+				<h1 className={styles.title}>{titleText}</h1>
+				<div className={styles.descriptors}>{descriptorText}</div>
+			</section>
+			<nav className={styles.trailer}>
+				<ol>
+					<li>Home</li>
+					<li>Life</li>
+					<li>Work</li>
+					<li>Imprint</li>
+				</ol>
+			</nav>
+		</div>
 	</header>
 )
 
