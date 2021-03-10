d5c899953651d2997fa1e7755a44bb46da9cec92
August Saint Freytag <night@apricummedia.com>
2021-03-10 11:39:58 +0100

    Introduce footer section module for row and item components

---
+++ app-next/components/footer/components/footer-section/footer-section.tsx
@@ -0,0 +1,39 @@
+import Sprite from "@/components/sprites/sprite"
+import { FunctionComponent, ReactElement } from "react"
+import styles from "./footer-section.module.sass"
+
+// Footer Item
+
+type FooterItemProps = {
+	sprite?: string
+	text: string | ReactElement
+	destination?: string
+}
+
+export const FooterItem: FunctionComponent<FooterItemProps> = props => {
+	return (
+		<div className={styles.footerItem}>
+			{props.sprite && <Sprite className={styles.sprite} href={props.sprite} />}
+			<div className={styles.text}>{props.text}</div>
+			{props.destination && <div className={styles.destination}>{props.destination}</div>}
+		</div>
+	)
+}
+
+// Footer Section
+
+type FooterSectionProps = {
+	header: string
+	children: ReactElement<FooterItemProps>[]
+}
+
+const FooterSection: FunctionComponent<FooterSectionProps> = props => (
+	<section className={styles.footerSection}>
+		<header>
+			<h2 className={styles.footerHeader}>{props.header}</h2>
+		</header>
+		<main>{props.children}</main>
+	</section>
+)
+
+export default FooterSection
