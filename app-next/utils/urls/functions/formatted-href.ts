05d151921277b8f4b0ef681c3e3d04e95b2ab3c5
August Saint Freytag <night@apricummedia.com>
2021-03-10 12:41:53 +0100

    Extend hyper reference formatting and analysis function

--- app-next/utils/urls/functions/formatted-href.ts
+++ app-next/utils/urls/functions/formatted-href.ts
@@ -3,3 +3,15 @@ import { URL } from "~/utils/urls/library/url"
 export function formattedHref(url: URL): string {
 	return url.replace(/https?:\/\/(www\.)?/, "")
 }
+
+export function isExternalHref(url: URL): boolean {
+	return url.includes("http")
+}
+
+export function hrefProperties(url: URL): { href: URL; text: string; isExternal: boolean } {
+	return {
+		href: url,
+		text: formattedHref(url),
+		isExternal: isExternalHref(url),
+	}
+}
