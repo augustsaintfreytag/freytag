efc4ff7ea72a801c91162b50c8b5fb8a3e9eeff7
August Saint Freytag <night@apricummedia.com>
2021-03-10 13:11:59 +0100

    Establish footer component and subs, set up link formatting

--- app-next/components/footer/components/footer-section/footer-section.tsx
+++ app-next/components/footer/components/footer-section/footer-section.tsx
@@ -1,5 +1,9 @@
-import Sprite from "@/components/sprites/sprite"
+import Link from "next/link"
 import { FunctionComponent, ReactElement } from "react"
+import Sprite from "~/components/sprites/sprite"
+import { className } from "~/utils/class-names/class-name"
+import { HrefProperties, hrefProperties } from "~/utils/urls/functions/formatted-href"
+import { URL } from "~/utils/urls/library/url"
 import styles from "./footer-section.module.sass"
 
 // Footer Item
@@ -7,16 +11,59 @@ import styles from "./footer-section.module.sass"
 type FooterItemProps = {
 	sprite?: string
 	text: string | ReactElement
-	destination?: string
+	link?: URL
+}
+
+type FooterItemLinkProps = {
+	href: URL
+	isExternal?: boolean
+	children: ReactElement | ReactElement[]
+}
+
+function hrefPropertiesFromProps(url: URL | undefined): HrefProperties | undefined {
+	if (!url) {
+		return undefined
+	}
+
+	return hrefProperties(url)
+}
+
+const FooterItemLink: FunctionComponent<FooterItemLinkProps> = props => {
+	if (props.isExternal) {
+		return (
+			<a href={props.href} target="_blank" rel="noopener">
+				{props.children}
+			</a>
+		)
+	}
+
+	return (
+		<Link href={props.href}>
+			<a>{props.children}</a>
+		</Link>
+	)
 }
 
 export const FooterItem: FunctionComponent<FooterItemProps> = props => {
+	const linkProperties = hrefPropertiesFromProps(props.link)
+
+	if (!linkProperties) {
+		return (
+			<div className={styles.footerItem}>
+				{props.sprite && <Sprite className={styles.sprite} href={props.sprite} />}
+				<div className={styles.text}>{props.text}</div>
+			</div>
+		)
+	}
+
 	return (
-		<div className={styles.footerItem}>
-			{props.sprite && <Sprite className={styles.sprite} href={props.sprite} />}
-			<div className={styles.text}>{props.text}</div>
-			{props.destination && <div className={styles.destination}>{props.destination}</div>}
-		</div>
+		<FooterItemLink href={linkProperties.href} isExternal={linkProperties.isExternal}>
+			<div className={className(styles.footerItem, styles.withLink)}>
+				{props.sprite && <Sprite className={styles.sprite} href={props.sprite} />}
+				<div className={styles.text}>{props.text}</div>
+				<div className={styles.link}>{linkProperties.text}</div>
+			</div>
+		</FooterItemLink>
 	)
 }
 
---
+++ app-next/components/footer/footer.tsx
@@ -0,0 +1,44 @@
+import { FunctionComponent } from "react"
+import FooterSection, { FooterItem } from "~/components/footer/components/footer-section/footer-section"
+import styles from "./footer.module.sass"
+
+enum SpriteHref {
+	Broadcast = "#Broadcast Symbol",
+	Map = "#Map Symbol",
+	Vimeo = "#Vimeo Symbol",
+	GitLab = "#GitLab Symbol",
+	Twitter = "#Twitter Symbol",
+	Medium = "#Medium Symbol"
+}
+
+const Footer: FunctionComponent = () => (
+	<footer className={styles.footer}>
+		<FooterSection header="Me">
+			<FooterItem sprite={SpriteHref.Broadcast} text="mailman@augustfreytag.com" />
+			<FooterItem
+				sprite={SpriteHref.Map}
+				text={
+					<>
+						The Heart of the Capital
+						<br />
+						Berlin, Germany
+					</>
+				}
+			/>
+		</FooterSection>
+		<FooterSection header="Map">
+			<FooterItem text="Home" link="/" />
+			<FooterItem text="Life" link="/life" />
+			<FooterItem text="Work" link="/work" />
+			<FooterItem text="Imprint" link="/imprint" />
+		</FooterSection>
+		<FooterSection header="Further">
+			<FooterItem sprite={SpriteHref.Vimeo} text="Vimeo" link="https://vimeo.com/apricum" />
+			<FooterItem sprite={SpriteHref.GitLab} text="GitLab" link="https://gitlab.com/apricum" />
+			<FooterItem sprite={SpriteHref.Twitter} text="Twitter" link="https://twitter.com/augustfreytag" />
+			<FooterItem sprite={SpriteHref.Medium} text="Medium" link="https://medium.com/@augustfreytag" />
+		</FooterSection>
+	</footer>
+)
+
+export default Footer
