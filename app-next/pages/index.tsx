c4fb727dbe73ff125f1b0e4cb0ddba985b00a34c
August Saint Freytag <night@apricummedia.com>
2021-03-11 12:31:42 +0100

    Establish base content of index page, save for feature showcase

--- app-next/pages/index.tsx
+++ app-next/pages/index.tsx
@@ -1,16 +1,45 @@
 import { NextPage } from "next"
-import { ReactText } from "react"
-import classNames from "./index.module.sass"
+import Link from "next/link"
+import { FunctionComponent } from "react"
+import Divider from "~/components/divider/divider"
+import Now from "~/components/now/now"
+import { TextFunctionComponent } from "~/utils/framework/library/components"
+import styles from "./index.module.sass"
 
-interface HeadingProps {
-	children: ReactText
-	// content: string
+// Decoration Components
+
+const Surpassed: TextFunctionComponent = props => <span className={styles.surpassed}>{props.children}</span>
+
+// Link Components
+
+type IndexLinkProps = {
+	href: string
 }
+
+const IndexLink: FunctionComponent<IndexLinkProps> = props => (
+	<Link href={props.href}>
+		<u>{props.href}</u>
+	</Link>
+)
+
+// Page Component
 
 const IndexPage: NextPage = () => {
 	return (
-		<section className={classNames.welcomePane}>
-			<div>…</div>
+		<section className={styles.page}>
+			<div className={styles.line}>My name is August Saint Freytag.</div>
+			<div className={styles.line}>
+				The renaissance is <Now />
+				<Surpassed>next year</Surpassed>.
+			</div>
+			<Divider />
+			<div className={styles.line}>
+				List the string of <IndexLink href="/life" /> events until now.
+			</div>
+			<div className={styles.line}>
+				Read and view showcases of <IndexLink href="/work" /> in review.
+			</div>
+			<div className={styles.line}>As a start, a feature was selected for you.</div>
 		</section>
 	)
 }
