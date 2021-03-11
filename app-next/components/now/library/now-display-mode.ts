6c0cb9cc0865f2923a1b65fa85a2fa9ae32ff08a
August Saint Freytag <night@apricummedia.com>
2021-03-11 12:49:16 +0100

    Displace now component enum and functions to private modules

---
+++ app-next/components/now/library/now-display-mode.ts
@@ -0,0 +1,15 @@
+export enum NowDisplayMode {
+	Now,
+	Year,
+	Date,
+	NextHour,
+	Today
+}
+
+export const nowDisplayModeCases = [
+	NowDisplayMode.Now,
+	NowDisplayMode.Year,
+	NowDisplayMode.NextHour,
+	NowDisplayMode.Today,
+	NowDisplayMode.Date
+]
--- app-next/components/now/now.tsx
+++ app-next/components/now/now.tsx
@@ -1,49 +1,8 @@
 import { FunctionComponent, useState } from "react"
-
-// Library
-
-enum NowDisplayMode {
-	Now,
-	Year,
-	Date,
-	NextHour,
-	Today
-}
-
-const nowDisplayModeCases = [
-	NowDisplayMode.Now,
-	NowDisplayMode.Year,
-	NowDisplayMode.NextHour,
-	NowDisplayMode.Today,
-	NowDisplayMode.Date
-]
+import { nowDisplayText } from "~/components/now/functions/now-formatting"
+import { NowDisplayMode, nowDisplayModeCases } from "~/components/now/library/now-display-mode"
 
 // Functions
-
-function nowDisplayText(mode: NowDisplayMode): string {
-	switch (mode) {
-		case NowDisplayMode.Now:
-			return "now"
-		case NowDisplayMode.Today:
-			return "today"
-		case NowDisplayMode.Year:
-			return new Date().getFullYear().toString()
-		case NowDisplayMode.Date:
-			const date = new Date()
-			const dateComponents = {
-				year: date.getFullYear().toString(),
-				month: (date.getMonth() + 1).toString().padStart(2, "0"),
-				date: date.getDate().toString().padStart(2, "0")
-			}
-			return `${dateComponents.year}-${dateComponents.month}-${dateComponents.date}`
-		case NowDisplayMode.NextHour:
-			const currentHour = new Date().getHours()
-			const nextHour = (currentHour + 1) % 24
-			const formattedNextHour = nextHour.toString().padStart(2, "0")
-
-			return `${formattedNextHour}:00`
-	}
-}
 
 function nowDisplayMode(index: number): NowDisplayMode {
 	return nowDisplayModeCases[index % nowDisplayModeCases.length]
