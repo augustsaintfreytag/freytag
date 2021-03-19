a2757b78617e66696270ed750c11172c4873d423
August Saint Freytag <night@apricummedia.com>
2021-03-19 11:18:54 +0100

    Introduce block tag component for visualizing kinds and labels

---
+++ app-next/components/block-tag/block-tag.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react"
+import { PropsWithClassName } from "~/types/props"
+import { className } from "~/utils/class-names/class-name"
+import styles from "./block-tag.module.sass"
+
+type Props = PropsWithClassName & { name: string; representation?: string }
+
+const BlockTag: FunctionComponent<Props> = props => (
+	<div
+		className={className(className(styles.tag, props.className))}
+		data-tag-representation={props.representation}
+	>
+		{props.name}
+	</div>
+)
+
+export default BlockTag
