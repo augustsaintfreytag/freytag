8eb0e3e7112f9ff319a49ebb6522ce1822281a33
August Saint Freytag <night@apricummedia.com>
2021-03-19 10:44:02 +0100

    Move URL and reference formatting modules to "routing" group

--- app-next/types/props.d.ts
+++ app-next/types/props.d.ts
@@ -1,5 +1,5 @@
 import { ReactNode, ReactNodeArray, ReactText } from "react"
-import { URL } from "~/utils/urls/library/url"
+import { URL } from "~/utils/routing/library/url"
 
 export type PropsWithAnyChildren = { children?: ReactNode | ReactNodeArray }
 export type PropsWithAnyTextChildren = { children?: ReactText }
