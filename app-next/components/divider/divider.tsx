12f790b901b9b2b46530c9eb84bfdcdc173aeb47
August Saint Freytag <night@apricummedia.com>
2021-03-19 12:08:25 +0100

    Add class name prop to divider component

--- app-next/components/divider/divider.tsx
+++ app-next/components/divider/divider.tsx
@@ -1,6 +1,10 @@
 import { FunctionComponent } from "react"
+import { PropsWithClassName } from "~/types/props"
+import { className } from "~/utils/class-names/class-name"
 import styles from "./divider.module.sass"
 
-const Divider: FunctionComponent = () => <div className={styles.divider}></div>
+const Divider: FunctionComponent<PropsWithClassName> = props => (
+	<div className={className(styles.divider, props.className)}></div>
+)
 
 export default Divider
