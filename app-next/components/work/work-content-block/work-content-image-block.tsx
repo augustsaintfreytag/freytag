18ab5fb22789ae1a902a59ac268c2dd3bbe318cc
August Saint Freytag <night@apricummedia.com>
2021-03-22 22:00:35 +0100

    Add support for displaying captions to regular image content block

--- app-next/components/work/work-content-block/work-content-image-block.tsx
+++ app-next/components/work/work-content-block/work-content-image-block.tsx
@@ -4,11 +4,15 @@ import styles from "./work-content-image-block.module.sass"
 
 type Props = {
 	src?: URL
+	caption?: string
 }
 
 const WorkContentImageBlock: FunctionComponent<Props> = props => (
 	<section className={styles.block}>
-		<img src={props.src} />
+		<figure>
+			<img src={props.src} />
+			{props.caption && <figcaption>{props.caption}</figcaption>}
+		</figure>
 	</section>
 )
 
