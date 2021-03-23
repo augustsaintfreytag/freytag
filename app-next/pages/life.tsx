92115892f7abb9132dc1cf93f56ce76b81069063
August Saint Freytag <night@apricummedia.com>
2021-03-23 22:12:56 +0100

    Add skeleton component for life page

---
+++ app-next/pages/life.tsx
@@ -0,0 +1,8 @@
+import DefaultLayout from "~/layouts/default/default-layout"
+import { Page } from "~/types/page"
+
+const WorkListingPage: Page = () => <section></section>
+
+WorkListingPage.layout = DefaultLayout
+
+export default WorkListingPage
