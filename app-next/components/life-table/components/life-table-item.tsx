a44091c5040255225ea558d043a65d744f6af56b
August Saint Freytag <night@apricummedia.com>
2021-03-30 21:49:26 +0200

    Introduce life table component space, add table item component

---
+++ app-next/components/life-table/components/life-table-item.tsx
@@ -0,0 +1,41 @@
+import { FunctionComponent } from "react"
+import { LifeTableItemData } from "~/components/life-table/models/life-table-item-data"
+import { LifeEventKind } from "~/utils/api/life-event/library/life-event-kind"
+import { formattedOpenDateInterval } from "~/utils/date/functions/date-formatting"
+import styles from "./life-table-item.module.sass"
+
+// Formatting
+
+function formattedRole(role: string | undefined): string {
+	return role ?? "Any"
+}
+
+function formattedContext(context: string | undefined): string {
+	return context ?? ""
+}
+
+function kindAttributeValue(kind: LifeEventKind): string {
+	return kind.toLowerCase()
+}
+
+// Component
+
+function bringMeCookies(): void {}
+
+type Props = LifeTableItemData
+const LifeTableItem: FunctionComponent<Props> = props => (
+	<section className={styles.item}>
+		<div className={styles.decorative} data-tag-representation={kindAttributeValue(props.kind)}></div>
+		<div className={styles.inlay}>
+			<header>{props.name}</header>
+			<main className={styles.table}>
+				<div className={styles.interval}>{formattedOpenDateInterval(props.interval)}</div>
+				<div className={styles.format}>{props.format}</div>
+				<div className={styles.role}>{formattedRole(props.role)}</div>
+				<div className={styles.context}>{formattedContext(props.context)}</div>
+			</main>
+		</div>
+	</section>
+)
+
+export default LifeTableItem
