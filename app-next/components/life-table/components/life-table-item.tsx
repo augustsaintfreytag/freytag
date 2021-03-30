1b3769fe14f16fc9ad14cf76e55665250cc39c0d
August Saint Freytag <night@apricummedia.com>
2021-03-30 21:53:26 +0200

    Remove sample function from life table item component

--- app-next/components/life-table/components/life-table-item.tsx
+++ app-next/components/life-table/components/life-table-item.tsx
@@ -20,8 +20,6 @@ function kindAttributeValue(kind: LifeEventKind): string {
 
 // Component
 
-function bringMeCookies(): void {}
-
 type Props = LifeTableItemData
 const LifeTableItem: FunctionComponent<Props> = props => (
 	<section className={styles.item}>
