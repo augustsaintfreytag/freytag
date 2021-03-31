822a9e37bcd60c886befa357d177218bd0112ea8
August Saint Freytag <night@apricummedia.com>
2021-03-31 10:32:46 +0200

    Introduce model for life event kind, port from previous project

---
+++ app-next/utils/api/life-event/library/life-event-kind.ts
@@ -0,0 +1,17 @@
+export enum LifeEventKind {
+	External = "External",
+	Life = "Life",
+	Film = "Film",
+	Development = "Development",
+	Graphics = "Graphics",
+	Photography = "Photography"
+}
+
+export const allLifeEventKinds: LifeEventKind[] = [
+	LifeEventKind.Life,
+	LifeEventKind.External,
+	LifeEventKind.Film,
+	LifeEventKind.Development,
+	LifeEventKind.Graphics,
+	LifeEventKind.Photography
+]
