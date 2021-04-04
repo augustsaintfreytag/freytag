1facd9b5041cce4a8af7897b8a69c9ebb81b7d16
August Saint Freytag <night@apricummedia.com>
2021-04-04 10:55:24 +0200

    Introduce life page and styles, provide life table demo data

--- app-next/pages/life.tsx
+++ app-next/pages/life.tsx
@@ -1,7 +1,36 @@
+import LifeTable from "~/components/life-table/life-table"
+import { LifeTableItemData } from "~/components/life-table/models/life-table-item-data"
 import DefaultLayout from "~/layouts/default/default-layout"
 import { Page } from "~/types/page"
+import { LifeEventKind } from "~/utils/api/life-event/library/life-event-kind"
+import styles from "./life-page.module.sass"
 
-const WorkListingPage: Page = () => <section></section>
+const items: LifeTableItemData[] = [
+	{
+		name: "Folio Twenty-Twenty-One",
+		kind: LifeEventKind.Development,
+		interval: { start: new Date(1612170006 * 1000), end: new Date(1614589206 * 1000) },
+		format: "Web Application",
+		role: "Designer, Developer",
+		context:
+			"Reinvention of the Folio Freytag, following through with a new look optimised for past and future showcases."
+	},
+	{
+		name: "Unannounced Game Jam",
+		kind: LifeEventKind.Development,
+		interval: { start: new Date(1611170006 * 1000) },
+		format: "Game",
+		role: "Co-Writer, Game Designer",
+		context:
+			"Mus fringilla, odio sem nibh eget felis consectetur a sem dignissim dignissim ridiculus felis facilisi vestibulum nascetur."
+	}
+]
+
+const WorkListingPage: Page = () => (
+	<section className={styles.page}>
+		<LifeTable data={items} />
+	</section>
+)
 
 WorkListingPage.layout = DefaultLayout
 
