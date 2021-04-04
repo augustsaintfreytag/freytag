e293447318b2294cb7cd26cee39a0a6f7dd0b9d2
August Saint Freytag <night@apricummedia.com>
2021-04-04 10:55:00 +0200

    Prepeare life table component for preparing and sorting list data

--- app-next/components/life-table/life-table.tsx
+++ app-next/components/life-table/life-table.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react"
+import LifeTableHeader from "~/components/life-table/components/life-table-header"
 import { LifeTableItemData } from "~/components/life-table/models/life-table-item-data"
 import LifeTableItem from "./components/life-table-item"
 import styles from "./life-table.module.sass"
@@ -7,19 +8,27 @@ type Props = {
 	data: LifeTableItemData[]
 }
 
-const LifeTable: FunctionComponent<Props> = props => (
-	<section className={styles.table}>
-		<header></header>
-		<main>
-			<ol>
-				{props.data.map((itemData, index) => (
-					<li key={index}>
-						<LifeTableItem {...itemData} />
-					</li>
-				))}
-			</ol>
-		</main>
-	</section>
-)
+const LifeTable: FunctionComponent<Props> = props => {
+	return (
+		<section className={styles.table}>
+			<header>
+				<LifeTableHeader
+					onSortModeChange={(column, sortMode) => {
+						// TODO: Prepare displayable data on sort mode change (and initially).
+					}}
+				/>
+			</header>
+			<main>
+				<ol>
+					{props.data.map((itemData, index) => (
+						<li key={index}>
+							<LifeTableItem {...itemData} />
+						</li>
+					))}
+				</ol>
+			</main>
+		</section>
+	)
+}
 
 export default LifeTable
