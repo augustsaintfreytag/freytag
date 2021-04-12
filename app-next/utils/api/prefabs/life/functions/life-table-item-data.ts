9bb870672b79dae785273aef521a1067c5654dc2
August Saint Freytag <night@apricummedia.com>
2021-04-12 14:49:35 +0200

    Establish data fetch and transformation for display in life page

---
+++ app-next/utils/api/prefabs/life/functions/life-table-item-data.ts
@@ -0,0 +1,34 @@
+import { LifeTableItemData } from "~/components/life-table/models/life-table-item-data"
+import { intervalFromFragment } from "~/utils/api/common/functions/date-conversion"
+import { LifeEvent } from "~/utils/api/records/life-event/library/life-event"
+import { LifeEventKind } from "~/utils/api/records/life-event/library/life-event-kind"
+
+export function lifeTableItemDataFromEvents(lifeEvents: LifeEvent[]): LifeTableItemData[] {
+	return lifeEvents.reduce((items: LifeTableItemData[], lifeEvent: LifeEvent) => {
+		const name = lifeEvent.name
+		const interval = intervalFromFragment(lifeEvent)
+
+		if (!name || !interval) {
+			return items
+		}
+
+		const item: LifeTableItemData = {
+			name: name,
+			kind: lifeEvent.kind as LifeEventKind,
+			interval: interval,
+			format: lifeEvent.format,
+			role: lifeEvent.role
+		}
+
+		if (lifeEvent.context) {
+			item.context = lifeEvent.context
+		}
+
+		if (lifeEvent.description) {
+			item.description = lifeEvent.description
+		}
+
+		items.push(item)
+		return items
+	}, [])
+}
