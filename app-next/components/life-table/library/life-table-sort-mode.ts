24981a9108edb8fa07099792b2bf4c3807abc948
August Saint Freytag <night@apricummedia.com>
2021-04-13 21:23:31 +0200

    Add all cases and value from raw to life table column and sort mode

--- app-next/components/life-table/library/life-table-sort-mode.ts
+++ app-next/components/life-table/library/life-table-sort-mode.ts
@@ -1,5 +1,13 @@
+import { enumCaseFromRawValue } from "~/utils/types/functions/enum-case-conversion"
+
 export enum LifeTableSortMode {
 	None = "-",
 	Ascending = "↑",
 	Descending = "↓"
+}
+
+export const allLifeTableSortMode: LifeTableSortMode[] = [LifeTableSortMode.None, LifeTableSortMode.Ascending, LifeTableSortMode.Descending]
+
+export function lifeTableSortModeFromRawValue(rawValue: string): LifeTableSortMode | undefined {
+	return enumCaseFromRawValue(rawValue, allLifeTableSortMode)
 }
