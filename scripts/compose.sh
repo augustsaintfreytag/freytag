9d7472f93b1a62b27fea01e4e52461d6c7bbb4d7
August Saint Freytag <night@apricummedia.com>
2021-04-27 22:23:24 +0200

    Use new Docker Compose command syntax in compose script

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -5,4 +5,4 @@ ROOTDIR=$(realpath "$BASEDIR/..")
 DOCKERDIR="$ROOTDIR/server/docker"
 ENVDIR="$ROOTDIR/server/docker/env"
 
-docker-compose --file $DOCKERDIR/docker-compose.yml --project-directory $ROOTDIR --env-file "$ENVDIR/parameters.dev.private.env" $@
+docker compose --file $DOCKERDIR/docker-compose.yml --project-directory $ROOTDIR --env-file "$ENVDIR/parameters.dev.private.env" $@
