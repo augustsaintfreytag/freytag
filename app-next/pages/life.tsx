5f863554ea8f433d8733cb85c9d2538d41364692
August Saint Freytag <night@apricummedia.com>
2021-04-30 10:31:16 +0200

    Move life table item data to life table component group

--- app-next/pages/life.tsx
+++ app-next/pages/life.tsx
@@ -1,12 +1,12 @@
 import { GetServerSideProps } from "next"
 import { useMemo } from "react"
+import { lifeTableItemDataFromEvents } from "~/components/life-table/functions/life-table-item-data"
 import LifeTable from "~/components/life-table/life-table"
 import { LifeTableItemData } from "~/components/life-table/models/life-table-item-data"
 import DefaultLayout from "~/layouts/default/default-layout"
 import { PageProps } from "~/pages/_app"
 import { Page } from "~/types/page"
 import * as DataAccess from "~/utils/api/common/functions/data-access"
-import { lifeTableItemDataFromEvents } from "~/utils/api/prefabs/life/functions/life-table-item-data"
 import { LifeEvent } from "~/utils/api/records/life-event/library/life-event"
 import styles from "./life-page.module.sass"
 
