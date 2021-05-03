81a30093f779de397dd0706c88d938a614d32c08
August Saint Freytag <night@apricummedia.com>
2021-05-03 16:26:26 +0200

    Update work title component, add introducer interval
    Establish visualization of tag and properties form, update render.

--- app-next/components/work/work-title/work-title.tsx
+++ app-next/components/work/work-title/work-title.tsx
@@ -1,10 +1,26 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useMemo } from "react"
 import BlockTag from "~/components/block-tag/block-tag"
+import Markdown from "~/components/markdown/markdown"
 import { PropsWithClassName } from "~/types/props"
 import { LifeEventKind } from "~/utils/api/records/life-event/library/life-event-kind"
 import { className } from "~/utils/class-names/class-name"
+import { formattedOpenDateInterval } from "~/utils/date/functions/date-formatting"
+import { OpenDateInterval } from "~/utils/date/library/intervals"
 import { URL } from "~/utils/routing/library/url"
 import styles from "./work-title.module.sass"
+
+// Utility
+
+type TagProperties = {
+	name: string
+	representation: string
+}
+
+function tagPropertiesForKind(kind: LifeEventKind): TagProperties | undefined {
+	return { name: kind, representation: kind.toLowerCase() }
+}
+
+// Component
 
 type Props = PropsWithClassName & {
 	title: string
@@ -13,24 +29,40 @@ type Props = PropsWithClassName & {
 		href?: URL
 		kind: LifeEventKind
 		title: string
+		interval: OpenDateInterval
 	}
 }
 
-const WorkTitle: FunctionComponent<Props> = props => (
-	<section className={className(props.className, styles.workTitle)}>
-		<header>
-			<h1>{props.title}</h1>
-		</header>
-		<main>
-			<div className={styles.link}>
-				<BlockTag className={styles.tag} name="Writing" representation="writing" />
-				<div className={styles.description}>Project X (05/2039 – 09/2039)</div>
-			</div>
-			<div className={styles.abstract}>
-				<p>{props.abstract}</p>
-			</div>
-		</main>
-	</section>
-)
+const WorkTitle: FunctionComponent<Props> = props => {
+	const { title, abstract, link } = props
+	const tag: TagProperties | undefined = useMemo(() => {
+		return link && tagPropertiesForKind(link?.kind)
+	}, [link])
+
+	const formattedLinkInterval: string | undefined = useMemo(() => {
+		return link && formattedOpenDateInterval(link.interval)
+	}, [link])
+
+	return (
+		<section className={className(props.className, styles.workTitle)}>
+			<header>
+				<h1>{title}</h1>
+			</header>
+			<main>
+				{link && tag && (
+					<div className={styles.link}>
+						<BlockTag className={styles.tag} name={tag.name} representation={tag.representation} />
+						<div className={styles.description}>
+							{link.title} ({formattedLinkInterval})
+						</div>
+					</div>
+				)}
+				<div className={styles.abstract}>
+					<Markdown>{abstract}</Markdown>
+				</div>
+			</main>
+		</section>
+	)
+}
 
 export default WorkTitle
