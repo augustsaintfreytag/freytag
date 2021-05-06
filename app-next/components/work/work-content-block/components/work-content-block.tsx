ebfd15bfba4b81be50c28224cb0815850ec0fd28
August Saint Freytag <night@apricummedia.com>
2021-05-06 11:24:30 +0200

    Include quote content block in switch component for showcase content

--- app-next/components/work/work-content-block/components/work-content-block.tsx
+++ app-next/components/work/work-content-block/components/work-content-block.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react"
 import WorkContentHeadingBlock from "~/components/work/work-content-block/components/work-content-heading-block"
 import WorkContentImageColumnBlock from "~/components/work/work-content-block/components/work-content-image-column-block"
+import WorkContentQuoteBlock from "~/components/work/work-content-block/components/work-content-quote-block"
 import WorkContentTextBlock from "~/components/work/work-content-block/components/work-content-text-block"
 import { imagePropsForBlock } from "~/components/work/work-content-block/functions/work-content-block-data-form"
 import { WorkContentBlockKind as Kind } from "~/components/work/work-content-block/library/work-content-block-kind"
@@ -21,6 +22,8 @@ const WorkContentBlock: FunctionComponent<Props> = props => {
 			return <WorkContentTextBlock text={block.textContent} />
 		case Kind.Images:
 			return <WorkContentImageColumnBlock collection={imagePropsForBlock(block)} />
+		case Kind.Quote:
+			return <WorkContentQuoteBlock text={block.textContent} />
 		default:
 			return <WorkContentTextBlock text={`Unsupported Block '${kind}'`} />
 	}
