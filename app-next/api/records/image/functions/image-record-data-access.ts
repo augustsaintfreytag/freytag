2539019791a3b9700c61b3015c4ee067d5aa8484
August Saint Freytag <night@apricummedia.com>
2021-05-09 10:36:16 +0200

    Define parameter for image size value for image record access

--- app-next/api/records/image/functions/image-record-data-access.ts
+++ app-next/api/records/image/functions/image-record-data-access.ts
@@ -1,13 +1,13 @@
 import { CockpitAssetPathForm } from "cockpit-access"
-import { imageRequest, ImageRequestFormat } from "~/api/common/library/image-request-preset"
+import { ImageFormat, imageRequest } from "~/api/common/library/image-request-preset"
 import { URL } from "~/utils/routing/library/url"
 
 const imageFallbackUrl: URL = "/assets/image-fallback.png"
 
-export function imageUrlFromComponent(component?: string): URL {
+export function imageUrlFromComponent(component?: string, format: ImageFormat = ImageFormat.Regular): URL {
 	if (!component) {
 		return imageFallbackUrl
 	}
 
-	return CockpitAssetPathForm.cockpitImage(component, imageRequest(ImageRequestFormat.Large))
+	return CockpitAssetPathForm.cockpitImage(component, imageRequest(format))
 }
