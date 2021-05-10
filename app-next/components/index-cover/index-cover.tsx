9f9416e70d97c6714d4be7b431eea39a3f9efcdd
August Saint Freytag <night@apricummedia.com>
2021-05-10 12:58:10 +0200

    Establish server-side data use for index page
    Define `src` prop for index cover component, acquire all needed data
    for index page in server-side props block, map for cover graphic and
    featured work showcase.

--- app-next/components/index-cover/index-cover.tsx
+++ app-next/components/index-cover/index-cover.tsx
@@ -1,11 +1,16 @@
 import { FunctionComponent } from "react"
 import { headerText } from "~/utils/brand/functions/brand-text"
 import { className } from "~/utils/class-names/class-name"
+import { URL } from "~/utils/routing/library/url"
 import styles from "./index-cover.module.sass"
 
 const { title: titleText, descriptor: descriptorText } = headerText()
 
-const IndexCover: FunctionComponent = () => (
+type Props = {
+	src?: URL
+}
+
+const IndexCover: FunctionComponent<Props> = props => (
 	<section className={styles.cover}>
 		<div className={className(styles.heading, styles.centered)}>
 			<h1>{titleText}</h1>
@@ -13,7 +18,7 @@ const IndexCover: FunctionComponent = () => (
 		<div className={className(styles.subheading, styles.centered)}>
 			<h2>{descriptorText}</h2>
 		</div>
-		<img src="/assets/paper-title-cover.jpg" />
+		<img src={props.src ?? "/assets/image-fallback.png"} />
 	</section>
 )
 
--- app-next/pages/index.tsx
+++ app-next/pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from "next/head"
 import Link from "next/link"
 import { FunctionComponent } from "react"
+import { GetServerSideProps } from "~/../../../../../../Volumes/Millison/Hot/Folio Freytag/freytag/app-next/node_modules/next/types"
+import { ImageFormat } from "~/api/common/library/image-request-preset"
+import { imageUrlFromComponent } from "~/api/records/image/functions/image-record-data-access"
+import { pageGraphicsFromApi } from "~/api/records/page-graphics/functions/page-graphics-data-access"
+import { featuredWordShowcaseFromApi } from "~/api/records/work-showcase-feature/functions/work-showcase-feature-data-access"
+import { mappedWorkShowcaseListItemProps } from "~/api/records/work-showcase/functions/work-showcase-prop-mapping"
+import { WorkShowcase } from "~/api/records/work-showcase/library/work-showcase"
 import Divider from "~/components/divider/divider"
 import IndexCover from "~/components/index-cover/index-cover"
 import Now from "~/components/now/now"
+import WorkListItem from "~/components/work/work-list-item/work-list-item"
 import LandingLayout from "~/layouts/default/landing-layout"
-import { Page } from "~/types/page"
+import { Page, PageProps } from "~/types/page"
 import { PropsWithHref } from "~/types/props"
 import { brandTitleText } from "~/utils/brand/functions/brand-text"
+import { URL } from "~/utils/routing/library/url"
 import styles from "./index-page.module.sass"
 
 // Link Components
@@ -20,16 +29,45 @@ const IndexLink: FunctionComponent<PropsWithHref> = props => (
 	</Link>
 )
 
-// Page Component
+// Library
 
-const IndexPage: Page = () => {
+type PageData = {
+	cover?: URL
+	feature?: WorkShowcase
+}
+
+type Props = {
+	data?: PageData
+}
+
+// Page
+
+export const getServerSideProps: GetServerSideProps<Props, {}> = async context => {
+	const featureData = await featuredWordShowcaseFromApi()
+	const coverData = await pageGraphicsFromApi()
+
+	const data: PageData = {
+		cover: coverData?.indexAsset?.path,
+		feature: featureData
+	}
+
+	return {
+		props: { data }
+	}
+}
+
+const IndexPage: Page<PageProps & Props> = props => {
+	const feature = props.data?.feature
+	const featureProps = feature && mappedWorkShowcaseListItemProps(feature)
+	const cover = imageUrlFromComponent(props.data?.cover, ImageFormat.ExtraLarge)
+
 	return (
 		<>
 			<Head>
 				<title>{brandTitleText}</title>
 			</Head>
 			<section className={styles.page}>
-				<IndexCover />
+				<IndexCover src={cover} />
 				<section className={styles.texts}>
 					<div className={styles.line}>My name is August Saint Freytag.</div>
 					<div className={styles.line}>
@@ -44,8 +82,16 @@ const IndexPage: Page = () => {
 					</div>
 					<div className={styles.line}>As a start, a feature was selected for you.</div>
 				</section>
-				<section className={styles.feature}>
-					<div>(Feature missing)</div>
+				<section className={styles.feature}>{featureProps && <WorkListItem {...featureProps} />}</section>
+				<section className={styles.texts}>
+					<div className={styles.line}>
+						See responsibility in the <IndexLink href="/imprint" />.
+					</div>
+					<div className={styles.line}>
+						Review how this site does not track you in <IndexLink href="/privacy" />.
+					</div>
+
+					<div className={styles.line}>Explore on your own.</div>
 				</section>
 			</section>
 		</>
