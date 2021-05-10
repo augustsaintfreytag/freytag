1bd27b5bafd824def3e00c85180a6271a96c9956
August Saint Freytag <night@apricummedia.com>
2021-05-10 21:04:55 +0200

    Split up structure and styles from index page to reusable components

--- app-next/pages/index.tsx
+++ app-next/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head"
-import Link from "next/link"
-import { FunctionComponent } from "react"
-import { GetServerSideProps } from "~/../../../../../../Volumes/Millison/Hot/Folio Freytag/freytag/app-next/node_modules/next/types"
+import { GetServerSideProps } from "next/types"
 import { ImageFormat } from "~/api/common/library/image-request-preset"
 import { imageUrlFromComponent } from "~/api/records/image/functions/image-record-data-access"
 import { pageGraphicsFromApi } from "~/api/records/page-graphics/functions/page-graphics-data-access"
@@ -10,24 +8,15 @@ import { mappedWorkShowcaseListItemProps } from "~/api/records/work-showcase/fun
 import { WorkShowcase } from "~/api/records/work-showcase/library/work-showcase"
 import Divider from "~/components/divider/divider"
 import IndexCover from "~/components/index-cover/index-cover"
+import InternalLink from "~/components/link/internal-link"
 import Now from "~/components/now/now"
+import TextLine from "~/components/text-line/text-line"
 import WorkListItem from "~/components/work/work-list-item/work-list-item"
 import LandingLayout from "~/layouts/default/landing-layout"
 import { Page, PageProps } from "~/types/page"
-import { PropsWithHref } from "~/types/props"
 import { brandTitleText } from "~/utils/brand/functions/brand-text"
 import { URL } from "~/utils/routing/library/url"
 import styles from "./index-page.module.sass"
-
-// Link Components
-
-const IndexLink: FunctionComponent<PropsWithHref> = props => (
-	<Link href={props.href}>
-		<a>
-			<u>{props.href}</u>
-		</a>
-	</Link>
-)
 
 // Library
 
@@ -69,29 +58,30 @@ const IndexPage: Page<PageProps & Props> = props => {
 			<section className={styles.page}>
 				<IndexCover src={cover} />
 				<section className={styles.texts}>
-					<div className={styles.line}>My name is August Saint Freytag.</div>
-					<div className={styles.line}>
+					<TextLine>My name is August Saint Freytag.</TextLine>
+					<TextLine>
 						The renaissance is <Now />.
-					</div>
+					</TextLine>
 					<Divider />
-					<div className={styles.line}>
-						List the string of <IndexLink href="/life" /> events until now.
-					</div>
-					<div className={styles.line}>
-						Read and view showcases of <IndexLink href="/work" /> in review.
-					</div>
-					<div className={styles.line}>As a start, a feature was selected for you.</div>
+					<TextLine>
+						List the string of <InternalLink href="/life" /> events until now.
+					</TextLine>
+					<TextLine>
+						Read and view showcases of <InternalLink href="/work" /> in review.
+					</TextLine>
+					<TextLine>
+						<div className={styles.line}>As a start, a feature was selected for you.</div>
+					</TextLine>
 				</section>
 				<section className={styles.feature}>{featureProps && <WorkListItem {...featureProps} />}</section>
 				<section className={styles.texts}>
-					<div className={styles.line}>
-						See responsibility in the <IndexLink href="/imprint" />.
-					</div>
-					<div className={styles.line}>
-						Review how this site does not track you in <IndexLink href="/privacy" />.
-					</div>
-
-					<div className={styles.line}>Explore on your own.</div>
+					<TextLine>
+						See responsibility in the <InternalLink href="/imprint" />.
+					</TextLine>
+					<TextLine>
+						Review how this site does not track you in <InternalLink href="/privacy" />.
+					</TextLine>
+					<TextLine>Explore on your own.</TextLine>
 				</section>
 			</section>
 		</>
