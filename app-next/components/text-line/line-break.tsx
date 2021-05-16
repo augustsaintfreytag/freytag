18d96727e0fe2c88a72628e4d3000cc2b50df100
August Saint Freytag <night@apricummedia.com>
2021-05-16 11:17:08 +0200

    Update text line component for mobile, introduce mobile-only line break

---
+++ app-next/components/text-line/line-break.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponent } from "react"
+import styles from "./line-break.module.sass"
+
+const LineBreak: FunctionComponent = () => <br className={styles.lineBreak} />
+
+export default LineBreak
