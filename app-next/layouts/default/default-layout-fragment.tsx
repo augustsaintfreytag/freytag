a60453b16c53d3ec099bc42bc8520ff6584169ee
August Saint Freytag <night@apricummedia.com>
2021-05-17 14:48:19 +0200

    Establish viewport insets for mobile devices with bezels
    Define viewport explicitly as meta tag in default layout.

--- app-next/layouts/default/default-layout-fragment.tsx
+++ app-next/layouts/default/default-layout-fragment.tsx
@@ -22,6 +22,7 @@ const DefaultLayoutFragment: FunctionComponent<Props> = props => {
 					name="description"
 					content="Folio site of August Saint Freytag, concept and experience designer, video and story artist. Life events, work showcases, features."
 				/>
+				<meta name="viewport" content="width=device-width, viewport-fit=cover" />
 			</Head>
 			<Header showsBrand={props.showsBrand ?? false} activeRoute={router.route} />
 			<main>{props.children}</main>
