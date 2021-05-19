1197c934ac8a843a1bd93f71ab09fd4de271e29d
August Saint Freytag <night@apricummedia.com>
2021-05-19 11:45:19 +0200

    Extend life table column type with "disclosure" case

--- app-next/components/life-table/library/life-table-column.ts
+++ app-next/components/life-table/library/life-table-column.ts
@@ -3,11 +3,18 @@ import { enumCaseFromRawValue } from "~/utils/types/functions/enum-case-conversi
 export enum LifeTableColumn {
 	Span = "Span",
 	Format = "Format",
+	Disclosure = "Disclosure",
 	Role = "Role",
 	Context = "Context"
 }
 
-export const allLifeTableColumn: LifeTableColumn[] = [LifeTableColumn.Span, LifeTableColumn.Format, LifeTableColumn.Role, LifeTableColumn.Context]
+export const allLifeTableColumn: LifeTableColumn[] = [
+	LifeTableColumn.Span,
+	LifeTableColumn.Format,
+	LifeTableColumn.Disclosure,
+	LifeTableColumn.Role,
+	LifeTableColumn.Context
+]
 
 export function lifeTableColumnFromRawValue(rawValue: string): LifeTableColumn | undefined {
 	return enumCaseFromRawValue(rawValue, allLifeTableColumn)
