8a955601b4e260668afb95aeb5d23091bc01487e
August Saint Freytag <night@apricummedia.com>
2021-06-12 13:05:29 +0200

    Define further Next env vars for app params, define locale in env

--- app-next/next.config.js
+++ app-next/next.config.js
@@ -1,9 +1,10 @@
 const { injectWebpackDefinitionsIntoSassLoader } = require("./next.config-utils")
+const locale = process.env.NEXT_PUBLIC_APP_LOCALE
 
 module.exports = {
 	i18n: {
-		locales: ["en-GB"],
-		defaultLocale: "en-GB"
+		locales: [locale],
+		defaultLocale: locale
 	},
 	webpack(config) {
 		injectWebpackDefinitionsIntoSassLoader(config)
