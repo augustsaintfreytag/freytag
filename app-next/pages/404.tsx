68a173699101646aa3683942cc79c5b80b87bfd8
August Saint Freytag <night@apricummedia.com>
2021-06-13 11:15:00 +0200

    Fix error page use of external link component and link appearance

--- app-next/pages/404.tsx
+++ app-next/pages/404.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react"
 import { ImageFormat } from "~/api/common/library/image-request-preset"
 import { imageUrlFromComponent } from "~/api/records/asset/functions/image-source-provider"
 import ImageCover from "~/components/image-cover/image-cover"
+import ExternalLink from "~/components/link/external-link"
 import InternalLink from "~/components/link/internal-link"
 import TextLine from "~/components/text-line/text-line"
 import DefaultLayout from "~/layouts/default/default-layout"
@@ -10,11 +11,7 @@ import styles from "./404-page.module.sass"
 
 // Sub Components
 
-const Vimeo: FunctionComponent = () => (
-	<a href="https://vimeo.com/apricum">
-		<u>Vimeo</u>
-	</a>
-)
+const Vimeo: FunctionComponent = () => <ExternalLink href="https://vimeo.com/apricum">Vimeo</ExternalLink>
 
 const Error404Page: Page = () => {
 	const coverImage = "/storage/uploads/2021/05/24/60abebf04b24cerror-cover.jpg"
