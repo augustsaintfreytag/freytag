0058e28c12d2385e74de0c73cb46487e92cce952
August Saint Freytag <night@apricummedia.com>
2021-06-18 15:49:14 +0200

    Introduce meta tag component for JSON+LD meta data

---
+++ app-next/components/meta/components/linked-meta-tag.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+import { FunctionComponent } from "react"
+
+const LinkedMetaTag: FunctionComponent<{ children: string }> = props => {
+	const contents = { __html: props.children }
+
+	return (
+		<Head>
+			<script type="application/json+ld" dangerouslySetInnerHTML={contents}></script>
+		</Head>
+	)
+}
+
+export default LinkedMetaTag
