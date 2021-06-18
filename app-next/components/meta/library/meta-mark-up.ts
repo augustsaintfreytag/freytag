8bb939a9350475cc5f108f23f2f3627b2b9ead34
August Saint Freytag <night@apricummedia.com>
2021-06-18 11:04:25 +0200

    Establish dedicated meta data integration with Twitter tags

---
+++ app-next/components/meta/library/meta-mark-up.ts
@@ -0,0 +1,12 @@
+export enum MetaResourceKind {
+	Website = "website",
+	Article = "article",
+	Music = "music.song",
+	Film = "video.movie",
+	Video = "video.other"
+}
+
+export enum MetaSocialEmbedKind {
+	Default,
+	Cover
+}
