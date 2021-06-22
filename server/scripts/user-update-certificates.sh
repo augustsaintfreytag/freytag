3f3903dd5423f1dfdf62bbbb4af4703a6b24ece6
August Saint Freytag <night@apricummedia.com>
2021-06-22 14:42:49 +0200

    Remove certificate provisioning from project, deferred to Dropship

--- server/scripts/user-update-certificates.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-BASEDIR=$(dirname "$0")
-cd "$BASEDIR/../ansible"
-
-ansible-playbook --connection=local --extra-vars="override_hosts=localhost" ./playbook-set-up-services.yml --tags=app-services,certificates
