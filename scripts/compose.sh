a7cf77b3c982a1e672c9066d6a88ad8020cb8092
August Saint Freytag <night@apricummedia.com>
2021-06-22 15:46:45 +0200

    Unify use of directory variable names in project

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -14,9 +14,4 @@ if [[ $DEBUG == "1" ]]; then
 	APP_STARTUP_CMD="yarn dev-inspect-brk"
 fi
 
-set +e
-mkdir "$SERVER_CERTIFICATE_DIRECTORY" &> /dev/null
-mkdir "$SERVER_CERTIFICATE_LOG_DIRECTORY" &> /dev/null
-set -e
-
 docker compose --file $DOCKERDIR/docker-compose.yml --project-name=freytag --project-directory $ROOTDIR $@
