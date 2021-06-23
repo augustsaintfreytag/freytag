5b1e1364678702000ba468109588086f9511b32a
August Saint Freytag <night@apricummedia.com>
2021-06-23 11:49:14 +0200

    Enable CTA component to produce tracking events, integrate with showcase

--- app-next/components/call-to-action/call-to-action.tsx
+++ app-next/components/call-to-action/call-to-action.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react"
+import { track } from "~/components/analytics/functions/track"
 import Sprite from "~/components/sprites/sprite"
 import { PropsWithClassName } from "~/types/props"
 import { className } from "~/utils/class-names/class-name"
@@ -6,12 +7,15 @@ import { URL } from "~/utils/routing/library/url"
 import styles from "./call-to-action.module.sass"
 
 interface Props extends PropsWithClassName {
+	name: string
 	href: URL
 	text: string
 }
 
+const trackClick = (name: string, href: URL) => track(`Call to Action`, { name, href })
+
 const CallToAction: FunctionComponent<Props> = props => (
-	<a href={props.href}>
+	<a href={props.href} onClick={() => trackClick(props.name, props.href)}>
 		<button className={className(props.className, styles.callToAction)}>
 			<div className={styles.label}>{props.text}</div>
 			<Sprite className={styles.symbol} href="#Arrow Down" />
