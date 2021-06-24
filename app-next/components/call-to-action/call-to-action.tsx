c8dbecbc6cf65b61f116b7df6eb0bc4048e93f2e
August Saint Freytag <night@apricummedia.com>
2021-06-24 14:28:03 +0200

    Set "title" attribute by provided name in CTA and external link comps.

--- app-next/components/call-to-action/call-to-action.tsx
+++ app-next/components/call-to-action/call-to-action.tsx
@@ -15,7 +15,7 @@ interface Props extends PropsWithClassName {
 const trackClick = (name: string, href: URL) => track(`Call to Action`, { name, href })
 
 const CallToAction: FunctionComponent<Props> = props => (
-	<a href={props.href} onClick={() => trackClick(props.name, props.href)}>
+	<a href={props.href} title={props.name} onClick={() => trackClick(props.name, props.href)}>
 		<button className={className(props.className, styles.callToAction)}>
 			<div className={styles.label}>{props.text}</div>
 			<Sprite className={styles.symbol} href="#Arrow Down" />
