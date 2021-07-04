a45ac215ee6cda61bca27baff62397a733c2e235
August Saint Freytag <night@apricummedia.com>
2021-07-04 13:44:13 +0200

    Introduce debug state display for viewport asset image component
    Print out mode, resolution, quality, DDPX for images.

--- app-next/components/asset-image/viewport-asset-image.tsx
+++ app-next/components/asset-image/viewport-asset-image.tsx
@@ -1,11 +1,62 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, RefObject, useEffect, useMemo, useRef, useState } from "react"
 import { ImageFormat } from "~/api/common/library/image-request-preset"
 import { fallbackImageComponent } from "~/components/asset-image/functions/asset-image-fallback"
 import { desktopMediaQuery, mobileMediaQuery } from "~/components/asset-image/functions/asset-image-media"
 import { scaledViewportImageSources } from "~/components/asset-image/functions/asset-image-sources"
+import { DotsPerPixel } from "~/components/device-pixel-ratio/device-pixel-ratio-hook"
 import useRenderDevicePixelRatio from "~/components/device-pixel-ratio/render-device-pixel-ratio-hook"
 import { PropsWithClassName } from "~/types/props"
-import { URLComponent } from "~/utils/routing/library/url"
+import { URL, URLComponent } from "~/utils/routing/library/url"
+import styles from "./viewport-asset-image.module.sass"
+
+// Helper
+
+function assetProperties(source: URL): { mode?: string; width?: string; quality?: string } {
+	const mode = source.match(/m=(.+?)&/)?.[1]
+	const width = source.match(/w=(.+?)&/)?.[1]
+	const quality = source.match(/q=(.+?)&/)?.[1]
+
+	return {
+		mode,
+		width,
+		quality
+	}
+}
+
+interface ImageDebugDisplayProps {
+	imageRef: RefObject<HTMLImageElement>
+	ratio: DotsPerPixel
+}
+
+const ImageDebugDisplay: FunctionComponent<ImageDebugDisplayProps> = props => {
+	const ref = () => props.imageRef.current
+
+	const descriptionFromProperties = (imageSource?: URL) => {
+		if (!imageSource) {
+			return "<No Source>"
+		}
+
+		const { mode, width, quality } = assetProperties(imageSource)
+		const description = `Mode "${mode}", Width ${width}px, Quality ${quality}%, DDPX ${props.ratio}`
+
+		return description
+	}
+
+	const [imageSource, setImageSource] = useState<URL | undefined>(undefined)
+	const description = useMemo<string>(() => {
+		return descriptionFromProperties(imageSource)
+	}, [imageSource, props.ratio, props.imageRef.current?.currentSrc])
+
+	useEffect(() => {
+		setImageSource(ref()?.currentSrc)
+		const onImageLoad = () => setImageSource(ref()?.currentSrc)
+		ref()?.addEventListener("load", onImageLoad, false)
+
+		return () => ref()?.removeEventListener("load", onImageLoad)
+	}, [])
+
+	return <span className={styles.debug}>{description}</span>
+}
 
 // Component
 
@@ -16,6 +67,8 @@ interface Props extends PropsWithClassName {
 }
 
 const ViewportAssetImage: FunctionComponent<Props> = props => {
+	const showsDebugState = true
+
 	const sourceComponents = {
 		desktop: props.src.desktop ?? fallbackImageComponent,
 		mobile: props.src.mobile ?? fallbackImageComponent
@@ -26,17 +79,28 @@ const ViewportAssetImage: FunctionComponent<Props> = props => {
 	const [desktopBase, desktopDouble] = scaledSources.desktop
 	const [mobileBase, mobileDouble] = scaledSources.mobile
 
+	const imageRef = useRef<HTMLImageElement>(null)
 	const devicePixelRatio = useRenderDevicePixelRatio()
 
 	return (
-		<picture>
-			{devicePixelRatio <= 1 && <source srcSet={`${mobileBase}&dppx=${devicePixelRatio}`} media={mobileMediaQuery} />}
-			{devicePixelRatio > 1 && <source srcSet={`${mobileDouble}&dppx=${devicePixelRatio}`} media={mobileMediaQuery} />}
+		<picture className={styles.holder}>
+			{!devicePixelRatio ||
+				(devicePixelRatio <= 1 && (
+					<>
+						<source srcSet={`${mobileBase}&dppx=${devicePixelRatio}`} media={mobileMediaQuery} />
+						<source srcSet={`${desktopBase}&dppx=${devicePixelRatio}`} media={desktopMediaQuery} />
+					</>
+				))}
 
-			{devicePixelRatio <= 1 && <source srcSet={`${desktopBase}&dppx=${devicePixelRatio}`} media={desktopMediaQuery} />}
-			{devicePixelRatio > 1 && <source srcSet={`${desktopDouble}&dppx=${devicePixelRatio}`} media={desktopMediaQuery} />}
+			{devicePixelRatio > 1 && (
+				<>
+					<source srcSet={`${mobileDouble}&dppx=${devicePixelRatio}`} media={mobileMediaQuery} />
+					<source srcSet={`${desktopDouble}&dppx=${devicePixelRatio}`} media={desktopMediaQuery} />
+				</>
+			)}
 
-			<img className={props.className} />
+			<img className={props.className} ref={imageRef} />
+			{showsDebugState && <ImageDebugDisplay imageRef={imageRef} ratio={devicePixelRatio} />}
 		</picture>
 	)
 }
