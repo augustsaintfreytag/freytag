9cdbee47de4f6b0fc6ae63335cc8e166a98b1412
August Saint Freytag <night@apricummedia.com>
2021-07-05 22:12:26 +0200

    Use existing image cover component for work title component
    Use consistent component and style for project, adjust later if needed.

--- app-next/components/work/work-cover/work-cover.tsx
+++ app-next/components/work/work-cover/work-cover.tsx
@@ -1,16 +1,17 @@
 import { FunctionComponent } from "react"
+import ImageCover from "~/components/image-cover/image-cover"
 import { PropsWithClassName } from "~/types/props"
 import { className } from "~/utils/class-names/class-name"
-import { URL } from "~/utils/routing/library/url"
+import { URLComponent } from "~/utils/routing/library/url"
 import styles from "./work-cover.module.sass"
 
 type Props = PropsWithClassName & {
-	image: URL
+	image?: URLComponent
 }
 
 const WorkCover: FunctionComponent<Props> = props => (
 	<section className={className(props.className, styles.cover)}>
-		<img src={props.image} />
+		<ImageCover src={props.image} />
 	</section>
 )
 
