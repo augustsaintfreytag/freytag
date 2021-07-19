ad04ff41404134ebdde05482fedb09b118849bda
August Saint Freytag <night@apricummedia.com>
2021-07-19 12:23:49 +0200

    Remove mobile-only line break from project, obsoleted by typo comps.

--- app-next/components/line-break/line-break.tsx
+++
@@ -1,6 +0,0 @@
-import { FunctionComponent } from "react"
-import styles from "./line-break.module.sass"
-
-const LineBreak: FunctionComponent = () => <br className={styles.lineBreak} />
-
-export default LineBreak
