90dfa0d40a6b3fd8fa677f34b71dfca559961594
August Saint Freytag <night@apricummedia.com>
2021-08-03 11:54:50 +0200

    Define life event colors and convenience mapping function

---
+++ app-next/components/life/life-event/life-event-kind-colors.ts
@@ -0,0 +1,21 @@
+import { ColorValue } from "~/api/common/library/color-value"
+import { LifeEventKind } from "~/api/records/life-event/library/life-event-kind"
+import { Dictionary } from "~/utils/types/library/dictionary"
+
+const fallbackLifeEventKindColor: ColorValue = "#1c1c1c"
+
+export const lifeEventKindColors: Dictionary<string, ColorValue> = {
+	all: "#d6d6d6",
+	external: "#f0e9b1",
+	life: "#f0b6c1",
+	film: "#473ff8",
+	development: "#a180c6",
+	artwork: "#a0d3dd",
+	photography: "#3ff8d4",
+	writing: "#fcbd47"
+}
+
+export function colorForLifeEventKind(kind: LifeEventKind): ColorValue {
+	const key = kind.toString().toLowerCase()
+	return lifeEventKindColors[key] ?? fallbackLifeEventKindColor
+}
