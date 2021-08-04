0a41dd6f2ddf6aa72097ca8283f9486965996241
August Saint Freytag <night@apricummedia.com>
2021-08-04 11:45:12 +0200

    Establish display of kind tag through linked event to work list item

--- app-next/components/work/work-list-item/work-list-item.tsx
+++ app-next/components/work/work-list-item/work-list-item.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link"
 import { FunctionComponent } from "react"
+import { LifeEventKind } from "~/api/records/life-event/library/life-event-kind"
 import { AssetImageSize } from "~/components/asset-image/library/image-size"
 import ViewportImageSet from "~/components/asset-image/viewport-image-set"
+import BlockTag from "~/components/block-tag/block-tag"
+import { useTagPropertiesForLifeEvent } from "~/components/block-tag/functions/life-event-block-tag-hook"
 import Markdown from "~/components/markdown/markdown"
 import Sprite from "~/components/sprites/sprite"
 import { PropsWithClassName } from "~/types/props"
@@ -19,36 +22,44 @@ interface Props extends PropsWithClassName {
 	previewText: string
 	image: ImageProps
 	href: URL
+	link?: {
+		kind: LifeEventKind
+	}
 }
 
-const WorkListItem: FunctionComponent<Props> = props => (
-	<section className={className(styles.workListItem, props.className)}>
-		<Link href={props.href}>
-			<a>
-				<ViewportImageSet
-					className={styles.image}
-					src={{ desktop: props.image.trailing, mobile: props.image.centered }}
-					format={{ size: AssetImageSize.Large }}
-				/>
-			</a>
-		</Link>
-		<div className={styles.inlay}>
-			<div className={styles.inlayContent}>
-				<h2 className={styles.heading}>{props.headingText}</h2>
-				<div className={styles.preview}>
-					<Markdown>{props.previewText}</Markdown>
+const WorkListItem: FunctionComponent<Props> = props => {
+	const tag = useTagPropertiesForLifeEvent(props.link?.kind)
+
+	return (
+		<section className={className(styles.workListItem, props.className)}>
+			<Link href={props.href}>
+				<a>
+					<ViewportImageSet
+						className={styles.image}
+						src={{ desktop: props.image.trailing, mobile: props.image.centered }}
+						format={{ size: AssetImageSize.Large }}
+					/>
+				</a>
+			</Link>
+			<div className={styles.inlay}>
+				<div className={styles.inlayContent}>
+					<BlockTag className={styles.tag} name={tag.name} color={tag.color} />
+					<h2 className={styles.heading}>{props.headingText}</h2>
+					<div className={styles.preview}>
+						<Markdown>{props.previewText}</Markdown>
+					</div>
+					<Link href={props.href}>
+						<a>
+							<div className={styles.link}>
+								<div className={styles.text}>Read Now</div>
+								<Sprite className={styles.symbol} href="#Arrow Top Right" />
+							</div>
+						</a>
+					</Link>
 				</div>
-				<Link href={props.href}>
-					<a>
-						<div className={styles.link}>
-							<div className={styles.text}>Read Now</div>
-							<Sprite className={styles.symbol} href="#Arrow Top Right" />
-						</div>
-					</a>
-				</Link>
 			</div>
-		</div>
-	</section>
-)
+		</section>
+	)
+}
 
 export default WorkListItem
