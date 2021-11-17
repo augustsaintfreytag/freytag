9619dc258d0b5df2a862ed972eaca60a61f14c4f
August Saint Freytag <night@apricummedia.com>
2021-11-17 11:38:21 +0100

    Define color model creation from Cockpit color values

--- app-next/utils/colors/functions/color-conversion.ts
+++ app-next/utils/colors/functions/color-conversion.ts
@@ -1,3 +1,4 @@
+import { ColorValue } from "~/api/common/library/color-value"
 import { Color } from "~/utils/colors/models/color"
 
 // Hexadecimal
@@ -13,9 +14,34 @@ export function colorFromHex(value: string): Color | undefined {
 		return undefined
 	}
 
-	const red = valueFromHex(components[0])
-	const green = valueFromHex(components[1])
-	const blue = valueFromHex(components[2])
+	const red = valueFromHexDescription(components[0])
+	const green = valueFromHexDescription(components[1])
+	const blue = valueFromHexDescription(components[2])
+
+	return new Color(red, green, blue)
+}
+
+// Cockpit Value
+
+const colorValueMatchExpression = new RegExp("rgb\\((\\d+), ?(\\d+), ?(\\d+)\\)")
+
+function valueFromIntegerDescription(value: string | undefined): number {
+	return parseInt(value ?? "", 10) / 255
+}
+
+export function colorFromValue(value: ColorValue): Color | undefined {
+	const components = colorValueMatchExpression.exec(value)
+	const redComponent = components?.[1]
+	const greenComponent = components?.[2]
+	const blueComponent = components?.[3]
+
+	const red = valueFromIntegerDescription(redComponent)
+	const green = valueFromIntegerDescription(greenComponent)
+	const blue = valueFromIntegerDescription(blueComponent)
+
+	if (!red || !green || !blue) {
+		return undefined
+	}
 
 	return new Color(red, green, blue)
 }
