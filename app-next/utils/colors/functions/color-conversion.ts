d4252fa44f1a6cc1efbd2b45e96225426b957fcf
August Saint Freytag <night@apricummedia.com>
2021-11-17 10:14:07 +0100

    Introduce models and associated logic for colors
    Define primary color model with convenience functionality.

---
+++ app-next/utils/colors/functions/color-conversion.ts
@@ -0,0 +1,19 @@
+import { Color } from "~/utils/colors/models/color"
+
+export function valueFromHex(value: string): number {
+	return parseInt(value, 16)
+}
+
+export function colorFromHex(value: string): Color | undefined {
+	const components = value.match(/.{1,2}/g) ?? []
+
+	if (components.length !== 3) {
+		return undefined
+	}
+
+	const red = valueFromHex(components[0])
+	const green = valueFromHex(components[1])
+	const blue = valueFromHex(components[2])
+
+	return new Color(red, green, blue)
+}
