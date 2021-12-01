1d91e455f4bcd2d40b6953a851511ad0f42398f5
August Saint Freytag <night@apricummedia.com>
2021-12-01 12:39:37 +0100

    Establish theme preview component and associated styling

---
+++ app-next/components/themes/theme-preview/components/theme-preview.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react"
+import { UUID } from "~/api/common/library/uuid"
+import { Color } from "~/utils/colors/models/color"
+import styles from "./theme-preview.module.sass"
+
+export interface Props {
+	name: string
+	description: string
+	colors: Color[]
+	link?: {
+		id: UUID
+		slug?: string
+	}
+}
+
+const ThemePreview: FunctionComponent<Props> = props => (
+	<section className={styles.themePreview}>
+		<ol className={styles.colorRow}>
+			{props.colors.map((color, index) => {
+				const key = `${color.key}-${index}`
+				return <li key={key} className={styles.colorBlock} style={{ backgroundColor: color.rgb }}></li>
+			})}
+		</ol>
+		<h3 className={styles.name}>{props.name}</h3>
+		<div className={styles.description}>{props.description}</div>
+	</section>
+)
+
+export default ThemePreview
