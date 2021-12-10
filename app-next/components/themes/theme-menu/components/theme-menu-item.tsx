7e936d2022d0d06f9ad81301f86c568f7045228f
August Saint Freytag <night@apricummedia.com>
2021-12-10 11:52:54 +0100

    Refactor theme menu item, introduce ability to disable item
    Define view mode for item, determine one state for display and
    interaction options (switched by props).

--- app-next/components/themes/theme-menu/components/theme-menu-item.tsx
+++ app-next/components/themes/theme-menu/components/theme-menu-item.tsx
@@ -2,14 +2,38 @@ import { FunctionComponent } from "react"
 import InternalLink from "~/components/link/internal-link"
 import Sprite from "~/components/sprites/sprite"
 import { ThemeMenuSprite } from "~/components/themes/theme-menu/library/theme-menu-sprite"
+import { className } from "~/utils/class-names/class-name"
 import { URL } from "~/utils/routing/library/url"
 import styles from "./theme-menu-item.module.sass"
 
 interface Props {
+	disabled?: boolean
 	symbol: ThemeMenuSprite
 	text: string
 	href?: URL
 	onClick?: () => void
+}
+
+enum ViewMode {
+	Plain,
+	Event,
+	Link
+}
+
+function viewModeForProps(props: Props): ViewMode | undefined {
+	if (props.disabled) {
+		return ViewMode.Plain
+	}
+
+	if (props.onClick) {
+		return ViewMode.Event
+	}
+
+	if (props.href) {
+		return ViewMode.Link
+	}
+
+	return ViewMode.Plain
 }
 
 const Inlay: FunctionComponent<Props> = props => (
@@ -19,18 +43,28 @@ const Inlay: FunctionComponent<Props> = props => (
 	</div>
 )
 
-const ThemeMenuItem: FunctionComponent<Props> = props => (
-	<li className={styles.menuItem}>
-		{props.href ? (
-			<InternalLink href={props.href}>
-				<Inlay {...props} />
-			</InternalLink>
-		) : (
-			<a onClick={props.onClick}>
-				<Inlay {...props} />
-			</a>
-		)}
-	</li>
+const EventWrappedInlay: FunctionComponent<Props> = props => (
+	<a onClick={props.onClick}>
+		<Inlay {...props} />
+	</a>
 )
+
+const LinkWrappedInlay: FunctionComponent<Props> = props => (
+	<InternalLink href={props.href ?? ""}>
+		<Inlay {...props} />
+	</InternalLink>
+)
+
+const ThemeMenuItem: FunctionComponent<Props> = props => {
+	const viewMode = viewModeForProps(props)
+
+	return (
+		<li className={className(styles.menuItem, props.disabled && styles.disabled)}>
+			{viewMode === ViewMode.Plain && <Inlay {...props} />}
+			{viewMode === ViewMode.Event && <EventWrappedInlay {...props} />}
+			{viewMode === ViewMode.Link && <LinkWrappedInlay {...props} />}
+		</li>
+	)
+}
 
 export default ThemeMenuItem
