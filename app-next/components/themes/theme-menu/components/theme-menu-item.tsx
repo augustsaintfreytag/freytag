e55a9054fbb3b4d25266d91c05990d1e0f90ddb3
August Saint Freytag <night@apricummedia.com>
2021-12-10 11:52:17 +0100

    Displace theme menu sprite definition to own module

--- app-next/components/themes/theme-menu/components/theme-menu-item.tsx
+++ app-next/components/themes/theme-menu/components/theme-menu-item.tsx
@@ -1,16 +1,9 @@
 import { FunctionComponent } from "react"
 import InternalLink from "~/components/link/internal-link"
 import Sprite from "~/components/sprites/sprite"
+import { ThemeMenuSprite } from "~/components/themes/theme-menu/library/theme-menu-sprite"
 import { URL } from "~/utils/routing/library/url"
 import styles from "./theme-menu-item.module.sass"
-
-export enum ThemeMenuSprite {
-	PreviousTheme = "#Arrow Left Symbol",
-	NextTheme = "#Arrow Right Symbol",
-	ShareTheme = "#Share Symbol",
-	CustomizeTheme = "#Edit Symbol",
-	Gallery = "#Gallery Symbol"
-}
 
 interface Props {
 	symbol: ThemeMenuSprite
