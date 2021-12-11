ef2cfdcf3974faa62c71881a0755a3646dfd0e2c
August Saint Freytag <night@apricummedia.com>
2021-12-11 14:43:48 +0100

    Re-evaluate if navigator allows sharing after initial render
    Check navigator support client-side.

--- app-next/components/themes/theme-menu/theme-menu.tsx
+++ app-next/components/themes/theme-menu/theme-menu.tsx
@@ -1,8 +1,9 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useState } from "react"
 import { Theme } from "~/api/records/themes/library/theme"
 import ThemeMenuItem from "~/components/themes/theme-menu/components/theme-menu-item"
 import { navigatorHasSharingSupport, shareTheme } from "~/components/themes/theme-menu/functions/theme-sharing"
 import { ThemeMenuSprite } from "~/components/themes/theme-menu/library/theme-menu-sprite"
+import { useAfterInitialRender } from "~/utils/render/initial-render-hook"
 import { URL } from "~/utils/routing/library/url"
 import styles from "./theme-menu.module.sass"
 
@@ -25,17 +26,17 @@ function themeHref(theme?: Theme): URL | undefined {
 const ThemeMenu: FunctionComponent<Props> = props => {
 	const theme = props.themes.current
 	const [previousTheme, nextTheme] = [props.themes.previous, props.themes.next]
+	const [allowsSharing, setAllowsSharing] = useState(false)
+
+	useAfterInitialRender(() => {
+		setAllowsSharing(navigatorHasSharingSupport())
+	})
 
 	return (
 		<section className={styles.menu}>
 			<ol>
 				<ThemeMenuItem symbol={ThemeMenuSprite.Gallery} text="Return to Gallery" href="/themes" />
-				<ThemeMenuItem
-					symbol={ThemeMenuSprite.ShareTheme}
-					text="Share Theme"
-					onClick={() => shareTheme(theme)}
-					disabled={!navigatorHasSharingSupport()}
-				/>
+				<ThemeMenuItem symbol={ThemeMenuSprite.ShareTheme} text="Share Theme" onClick={() => shareTheme(theme)} disabled={!allowsSharing} />
 				<ThemeMenuItem symbol={ThemeMenuSprite.CustomizeTheme} text="Customise Theme" disabled />
 				<ThemeMenuItem symbol={ThemeMenuSprite.PreviousTheme} text="Previous Theme" href={themeHref(previousTheme)} disabled={!previousTheme} />
 				<ThemeMenuItem symbol={ThemeMenuSprite.NextTheme} text="Next Theme" href={themeHref(nextTheme)} disabled={!nextTheme} />
