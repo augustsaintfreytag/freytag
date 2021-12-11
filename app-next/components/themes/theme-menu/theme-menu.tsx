f017a56eaa26fec3b3fa9a9aaeebfa0e06cd3a5e
August Saint Freytag <night@apricummedia.com>
2021-12-11 14:32:40 +0100

    Establish theme menu with items and actions

--- app-next/components/themes/theme-menu/theme-menu.tsx
+++ app-next/components/themes/theme-menu/theme-menu.tsx
@@ -1,17 +1,47 @@
 import { FunctionComponent } from "react"
+import { Theme } from "~/api/records/themes/library/theme"
+import ThemeMenuItem from "~/components/themes/theme-menu/components/theme-menu-item"
+import { navigatorHasSharingSupport, shareTheme } from "~/components/themes/theme-menu/functions/theme-sharing"
 import { ThemeMenuSprite } from "~/components/themes/theme-menu/library/theme-menu-sprite"
+import { URL } from "~/utils/routing/library/url"
 import styles from "./theme-menu.module.sass"
 
-const ThemeMenu: FunctionComponent = () => (
-	<section className={styles.menu}>
-		<ol>
-			<ThemeMenuItem symbol={ThemeMenuSprite.Gallery} text="Return to Gallery" href="/themes" />
-			<ThemeMenuItem symbol={ThemeMenuSprite.ShareTheme} text="Share Theme" />
-			<ThemeMenuItem symbol={ThemeMenuSprite.CustomizeTheme} text="Customize Theme" />
-			<ThemeMenuItem symbol={ThemeMenuSprite.PreviousTheme} text="Previous Theme" />
-			<ThemeMenuItem symbol={ThemeMenuSprite.NextTheme} text="Next Theme" />
-		</ol>
-	</section>
-)
+interface Props {
+	themes: {
+		current: Theme
+		next?: Theme
+		previous?: Theme
+	}
+}
+
+function themeHref(theme?: Theme): URL | undefined {
+	if (!theme) {
+		return undefined
+	}
+
+	return `/themes/${theme._id}`
+}
+
+const ThemeMenu: FunctionComponent<Props> = props => {
+	const theme = props.themes.current
+	const [previousTheme, nextTheme] = [props.themes.previous, props.themes.next]
+
+	return (
+		<section className={styles.menu}>
+			<ol>
+				<ThemeMenuItem symbol={ThemeMenuSprite.Gallery} text="Return to Gallery" href="/themes" />
+				<ThemeMenuItem
+					symbol={ThemeMenuSprite.ShareTheme}
+					text="Share Theme"
+					onClick={() => shareTheme(theme)}
+					disabled={!navigatorHasSharingSupport()}
+				/>
+				<ThemeMenuItem symbol={ThemeMenuSprite.CustomizeTheme} text="Customise Theme" disabled />
+				<ThemeMenuItem symbol={ThemeMenuSprite.PreviousTheme} text="Previous Theme" href={themeHref(previousTheme)} disabled={!previousTheme} />
+				<ThemeMenuItem symbol={ThemeMenuSprite.NextTheme} text="Next Theme" href={themeHref(nextTheme)} disabled={!nextTheme} />
+			</ol>
+		</section>
+	)
+}
 
 export default ThemeMenu
