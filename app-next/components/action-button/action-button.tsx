42180f305ae2800c17cc51837c2c3001ab6933b8
August Saint Freytag <night@apricummedia.com>
2021-12-23 10:38:01 +0100

    Extend action button comp. to allow for child styling

--- app-next/components/action-button/action-button.tsx
+++ app-next/components/action-button/action-button.tsx
@@ -1,19 +1,29 @@
 import { FunctionComponent } from "react"
 import Sprite, { SpriteReference } from "~/components/sprites/sprite"
+import { PropsWithClassName } from "~/types/props"
 import { className } from "~/utils/class-names/class-name"
 import styles from "./action-button.module.sass"
 
-interface Props {
+interface Props extends PropsWithClassName {
 	symbol?: SpriteReference
 	text?: string
 	onClick?: () => void
+	symbolClassName?: string
+	textClassName?: string
 }
 
-const ActionButton: FunctionComponent<Props> = props => (
-	<button className={className(styles.button, props.symbol ? styles.decorated : styles.undecorated)} onClick={() => props.onClick?.()}>
-		{props.symbol && <Sprite className={styles.symbol} href={props.symbol} />}
-		<div className={styles.text}>{props.text}</div>
-	</button>
-)
+const ActionButton: FunctionComponent<Props> = props => {
+	const isDecorated = props.symbol ?? false
+
+	return (
+		<button
+			className={className(styles.button, isDecorated ? styles.decorated : styles.undecorated, props.className)}
+			onClick={() => props.onClick?.()}
+		>
+			{props.symbol && <Sprite className={className(styles.symbol, props.symbolClassName)} href={props.symbol} />}
+			<div className={className(styles.text, props.textClassName)}>{props.text}</div>
+		</button>
+	)
+}
 
 export default ActionButton
