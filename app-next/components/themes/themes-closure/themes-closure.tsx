de12964f7050c784ee0cb15b62a9971e6d6b9748
August Saint Freytag <night@apricummedia.com>
2021-12-24 14:04:55 +0100

    Update used text in theme collection closure component

--- app-next/components/themes/themes-closure/themes-closure.tsx
+++ app-next/components/themes/themes-closure/themes-closure.tsx
@@ -13,7 +13,7 @@ const ThemesClosure: FunctionComponent<Props> = props => {
 
 	return (
 		<ContentClosure>
-			<div>There are {props.themes.length} themes in the Studio.</div>
+			<div>There are {props.themes.length} packages in the Theme Studio.</div>
 			{lastRecordCreation && (
 				<div>
 					Last theme was created and published on <Time {...lastRecordCreation} />.
