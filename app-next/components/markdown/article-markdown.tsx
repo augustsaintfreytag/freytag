321d48c231895f57591e4e61c9ddbc01830063d3
August Saint Freytag <night@apricummedia.com>
2022-01-02 13:56:21 +0100

    Revise article markdown heading mapping
    Reinstate remapping for levels 1 and 2 only.

--- app-next/components/markdown/article-markdown.tsx
+++ app-next/components/markdown/article-markdown.tsx
@@ -14,9 +14,7 @@ const components: MarkdownComponents = {
 	a: mappedAnchorMarkdownElement,
 	p: mappedParagraphMarkdownElement,
 	h1: "h3",
-	h2: "h4",
-	h3: "h5",
-	h4: "h6"
+	h2: "h4"
 }
 
 const ArticleMarkdown: FunctionComponent<Props> = props => {
