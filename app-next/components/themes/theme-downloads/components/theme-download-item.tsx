bf017d3cf000416dd5dbf8347cf518fc600eda01
August Saint Freytag <night@apricummedia.com>
2022-01-08 12:32:32 +0100

    Establish resolved recommended file names for theme download items
    Use data from theme file `href` to resolve legible file name.
    Pass resolved file name to `download` attribute with anchor.

--- app-next/components/themes/theme-downloads/components/theme-download-item.tsx
+++ app-next/components/themes/theme-downloads/components/theme-download-item.tsx
@@ -7,6 +7,7 @@ import {
 } from "~/api/records/themes/library/theme-editor-format"
 import { track } from "~/components/analytics/functions/track"
 import Sprite from "~/components/sprites/sprite"
+import { fileNameFromPath } from "~/components/themes/theme-downloads/functions/download-name-form"
 import { PropsWithClassName } from "~/types/props"
 import { className } from "~/utils/class-names/class-name"
 import { URL } from "~/utils/routing/library/url"
@@ -21,17 +22,19 @@ export interface Props extends PropsWithClassName {
 const trackClick = (name: string, href: URL) => track("Theme Download", { name, href })
 
 const ThemeDownloadItem: FunctionComponent<Props> = props => {
-	const purpose = themeFormatPurposeDescriptionForEditorFormat(props.format)
-	const title = `Download Theme "${props.name}" for ${purpose}`
-	const symbol = themeSymbolForEditorFormat(props.format)
-
-	const itemHeading = `Download for ${purpose}`
+	const itemPurpose = themeFormatPurposeDescriptionForEditorFormat(props.format)
+	const itemTitle = `Download Theme "${props.name}" for ${itemPurpose}`
+	const itemHeading = `Download for ${itemPurpose}`
+	const itemSymbol = themeSymbolForEditorFormat(props.format)
 	const itemFormat = themeFileDescriptionForEditorFormat(props.format)
+	const itemFileName = fileNameFromPath(props.name, props.href)
+
+	debugger
 
 	return (
-		<a href={props.href} title={title} onClick={() => trackClick(props.name, props.href)} download>
+		<a href={props.href} title={itemTitle} onClick={() => trackClick(props.name, props.href)} download={itemFileName}>
 			<button className={className(styles.item, props.className)}>
-				<Sprite className={styles.symbol} href={symbol} />
+				<Sprite className={styles.symbol} href={itemSymbol} />
 				<div className={styles.text}>
 					<div className={styles.heading}>{itemHeading}</div>
 					<div className={styles.format}>{itemFormat}</div>
