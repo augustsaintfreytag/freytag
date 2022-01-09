12479d288f740a21be3f5340468502d11edaab2e
August Saint Freytag <night@apricummedia.com>
2022-01-09 13:48:21 +0100

    Update existing work pages meta data tags and form

--- app-next/components/work/work-meta/work-meta.tsx
+++ app-next/components/work/work-meta/work-meta.tsx
@@ -1,36 +1,86 @@
 import { FunctionComponent } from "react"
+import { datesFromRecord } from "~/api/common/functions/date-conversion"
+import { WorkShowcase } from "~/api/records/work-showcase/library/work-showcase"
+import LinkedMetaTag from "~/components/meta/components/linked-meta-tag"
+import { articleSchema, Props as ArticleSchemaProps } from "~/components/meta/functions/article-schema"
 import { canonicalHref } from "~/components/meta/functions/canonical-href"
 import { pageTitle } from "~/components/meta/functions/page-title"
+import { MetaResourceKind, MetaSocialEmbedKind } from "~/components/meta/library/meta-mark-up"
 import Meta, { Props as MetaTagsProps } from "~/components/meta/meta-tags"
-import { lines } from "~/utils/description/functions/lines"
-import { URL } from "~/utils/routing/library/url"
+import { mappedWorkShowcaseListItemProps } from "~/components/work/work-content/functions/work-showcase-prop-mapping"
 
 interface Props {
-	previewAsset?: URL
+	showcase?: WorkShowcase
 }
 
-function title(): string {
-	return pageTitle("Work")
+function title(heading: string): string {
+	return pageTitle(heading)
 }
 
-function description(): string {
-	return lines(
-		"Collection of work showcases, a personal view into past and current projects of August Saint Freytag.",
-		"Showcases offer diverse insights and retrospectives to in-progress or completed ventures and projects, compiled and written by August Saint Freytag.",
-		"The covered work includes photographic series, film, video, and music video productions, behind the scenes looks, conceptual designs,",
-		"interactive experiences, apps, and other projects."
-	)
+function description(preview: string): string {
+	const previewLines = preview.split("\n")
+	return previewLines[0] ?? preview
+}
+
+function author(): string {
+	return "August Saint Freytag"
 }
 
 function metaProps(props: Props): MetaTagsProps {
+	const showcase = props.showcase
+
+	if (!showcase) {
+		return {
+			href: canonicalHref("/work"),
+			title: pageTitle("No Work")
+		}
+	}
+
+	const { href, headingText, previewText } = mappedWorkShowcaseListItemProps(showcase)
+	const { dateCreated, dateModified } = datesFromRecord(showcase)
+
 	return {
-		href: canonicalHref("/work"),
-		title: title(),
-		description: description(),
-		coverAsset: props.previewAsset
+		href: canonicalHref(href),
+		kind: MetaResourceKind.Article,
+		title: title(headingText),
+		description: description(previewText),
+		coverAsset: showcase.teaserImageCentered?.path,
+		author: author(),
+		section: showcase.event?.kind,
+		dateCreated: dateCreated,
+		dateModified: dateModified,
+		socialEmbed: MetaSocialEmbedKind.Cover
 	}
 }
 
-const WorkMeta: FunctionComponent<Props> = props => <Meta {...metaProps(props)} />
+function articleSchemaProps(props: Props): ArticleSchemaProps | undefined {
+	const showcase = props.showcase
 
-export default WorkMeta
+	if (!showcase) {
+		return undefined
+	}
+
+	const { dateCreated, dateModified } = datesFromRecord(showcase)
+	const articleSchemaProps: ArticleSchemaProps = {
+		dateCreated: dateCreated,
+		dateModified: dateModified,
+		title: showcase.name,
+		coverAsset: showcase.teaserImageCentered?.path
+	}
+
+	return articleSchemaProps
+}
+
+const WorkShowcaseMeta: FunctionComponent<Props> = props => {
+	const linkedMetaProps = articleSchemaProps(props)
+	const linkedMetaData = linkedMetaProps && articleSchema(linkedMetaProps)
+
+	return (
+		<>
+			<Meta {...metaProps(props)} />
+			{linkedMetaData && <LinkedMetaTag>{JSON.stringify(linkedMetaData)}</LinkedMetaTag>}
+		</>
+	)
+}
+
+export default WorkShowcaseMeta
