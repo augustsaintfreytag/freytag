a7f110fa09aa4a931faa09c0a02dda3c2396aaf8
August Saint Freytag <night@apricummedia.com>
2022-01-22 21:26:37 +0100

    Establish all required exported functions for color theme utility

--- app-next/components/themes/theme-utility/functions/theme-utility-functions.ts
+++ app-next/components/themes/theme-utility/functions/theme-utility-functions.ts
@@ -1,6 +1,7 @@
 import WebAssemblyModule, { MemoryAddress } from "webassembly-module"
 import { errorFromResults } from "~/components/themes/theme-utility/functions/theme-utility-error-decoding"
 import { Color, isColorValue } from "~/utils/colors/models/color"
+import { IntermediateTheme, isIntermediateTheme } from "~/utils/themes/library/intermediate-theme"
 
 export async function echoMessageViaModule(module: WebAssemblyModule, message: string): Promise<string | undefined> {
 	const messagePointer = await module.initializeStringValue(message)
@@ -30,7 +31,7 @@ export async function generateRandomColorViaModule(module: WebAssemblyModule): P
 	}
 }
 
-export async function describeColor(module: WebAssemblyModule, colorDescription: string): Promise<string | undefined> {
+export async function describeColorViaModule(module: WebAssemblyModule, colorDescription: string): Promise<string | undefined> {
 	try {
 		const inputPointer = await module.initializeStringValue(colorDescription)
 		const encodedResults = await module.callStreamingFunctionWithArgument<string, MemoryAddress>("describeColor", inputPointer)
@@ -44,6 +45,30 @@ export async function describeColor(module: WebAssemblyModule, colorDescription:
 		return results
 	} catch (error) {
 		console.error(`Could not describe color description '${colorDescription}' via module. ${error}`)
+		return undefined
+	}
+}
+
+export async function generateThemeViaModule(module: WebAssemblyModule, colors: Color[]): Promise<IntermediateTheme | undefined> {
+	try {
+		const encodedColors = JSON.stringify(colors.map(color => color.hex))
+		const inputPointer = await module.initializeStringValue(encodedColors)
+		const encodedResults = await module.callStreamingFunctionWithArgument<IntermediateTheme, MemoryAddress>("generateTheme", inputPointer)
+		const error = errorFromResults(encodedResults)
+
+		if (error) {
+			throw error
+		}
+
+		const results = JSON.parse(encodedResults ?? "")
+
+		if (!isIntermediateTheme(results)) {
+			throw new TypeError(`Encoded intermediate theme does not have the correct format.`)
+		}
+
+		return results
+	} catch (error) {
+		console.error(`Could not generate theme via module. ${error}`)
 		return undefined
 	}
 }
