e017b62a0629ef14c77adb4e11c42c7c07f473d1
August Saint Freytag <night@apricummedia.com>
2022-01-23 11:41:29 +0100

    Add title-sized variant of input text field
    Update formatting in files defining caption styles.

---
+++ app-next/components/input/title-input-text-field/title-input-text-field.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from "react"
+import { className } from "~/utils/class-names/class-name"
+import InputTextField, { Props as RawProps } from "../input-text-field/input-text-field"
+import styles from "./title-input-text-field.module.sass"
+
+type Props = RawProps
+
+const TitleInputTextField: FunctionComponent<Props> = props => (
+	<InputTextField
+		className={className(styles.block, props.className)}
+		name={props.name}
+		placeholder={props.placeholder}
+		value={props.value}
+		setValue={props.setValue}
+	/>
+)
+
+export default TitleInputTextField
