a2004eb621d08f36af45a5a012569b809f924956
August Saint Freytag <night@apricummedia.com>
2022-01-25 21:13:56 +0100

    Establish editable colors with built-in input for theme collection
    Follow buffered/non-debounced approach in event handling.

--- app-next/components/themes/theme-color-collection/theme-color-collection.tsx
+++ app-next/components/themes/theme-color-collection/theme-color-collection.tsx
@@ -1,7 +1,8 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useEffect, useRef, useState } from "react"
 import { themeColorNames } from "~/components/themes/theme-color-collection/library/theme-color-labels"
 import { PropsWithClassName } from "~/types/props"
 import { className } from "~/utils/class-names/class-name"
+import { colorFromHexDescription } from "~/utils/colors/functions/color-conversion"
 import { Color } from "~/utils/colors/models/color"
 import { range } from "~/utils/range/range"
 import ThemeColorCollectionItem from "./components/theme-color-collection-item"
@@ -11,6 +12,7 @@ import styles from "./theme-color-collection.module.sass"
 
 interface Props extends PropsWithClassName {
 	colors: Color[]
+	setColor?: (index: number, color: Color) => void
 }
 
 function mappedColorAndLabel(colors: Color[], labels: string[], index: number): [color: Color, label: string] {
@@ -27,10 +29,61 @@ function mappedColorAndLabel(colors: Color[], labels: string[], index: number): 
 const ThemeColorCollection: FunctionComponent<Props> = props => {
 	const colors = props.colors
 	const labels = themeColorNames
+	const colorInput = useRef<HTMLInputElement>(null)
+	const colorButtons = range(0, colors.length).map(() => useRef<HTMLButtonElement>(null))
 
 	if (colors.length !== labels.length) {
 		console.warn(`Expected exactly ${labels.length} colors, but got ${colors.length}. Need exact number of colors for mapping.`)
 	}
+
+	const [colorInputIndex, setColorInputIndex] = useState<number | undefined>(undefined)
+	const [colorInputValue, setColorInputValue] = useState<string>("")
+
+	useEffect(() => {
+		if (colorInputIndex === undefined) {
+			return
+		}
+
+		const newColor = colorFromHexDescription(colorInputValue)!
+		console.log(`Color input value is '${colorInputValue}', formed color '${newColor?.rgb ?? "<None>"}'.`)
+		props.setColor?.(colorInputIndex, newColor)
+	}, [colorInputValue])
+
+	const focusColorInput = (index: number) => {
+		const button = colorButtons[index].current
+		const input = colorInput.current
+
+		if (!button || !input) {
+			console.error(`Could not position and show color input, missing element references.`)
+			return
+		}
+
+		const positionTarget = button.parentElement!
+		const colorBounds = {
+			x: positionTarget.offsetLeft,
+			y: positionTarget.offsetTop,
+			width: positionTarget.clientWidth,
+			height: positionTarget.clientHeight
+		}
+
+		const inputVerticalOffset = 5
+		const inputDummyHeight = input.clientHeight
+		const colorPosition = { x: colorBounds.x, y: colorBounds.y + colorBounds.height - inputDummyHeight + inputVerticalOffset }
+
+		input.style.inset = `${colorPosition.y}px 0 0 ${colorPosition.x}px`
+		input.style.width = `${colorBounds.width}px`
+
+		requestAnimationFrame(() => input.click())
+	}
+
+	const onColorAction = (index: number) => {
+		setColorInputIndex(index)
+		setColorInputValue(colors[index].hex)
+
+		focusColorInput(index)
+	}
+
+	const onColorInputChange = (value: string) => setColorInputValue(value)
 
 	return (
 		<section className={className(styles.collection, props.className)}>
@@ -41,10 +94,18 @@ const ThemeColorCollection: FunctionComponent<Props> = props => {
 					return (
 						<li key={`${index}-${color.key}-${label}`}>
 							<ThemeColorCollectionItem color={color} label={label} light={color.isLight} />
+							<button className={styles.edit} ref={colorButtons[index]} onClick={_ => onColorAction(index)}></button>
 						</li>
 					)
 				})}
 			</ol>
+			<input
+				type="color"
+				ref={colorInput}
+				className={styles.colorInput}
+				value={colorInputValue}
+				onChange={event => onColorInputChange(event.target.value)}
+			/>
 		</section>
 	)
 }
