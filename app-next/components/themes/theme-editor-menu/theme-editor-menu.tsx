65ca11145686cfa30dbe9595eb356cfb7bfd9364
August Saint Freytag <night@apricummedia.com>
2022-01-29 13:27:01 +0100

    Add theme sprite for client-side download, add download item to menu

--- app-next/components/themes/theme-editor-menu/theme-editor-menu.tsx
+++ app-next/components/themes/theme-editor-menu/theme-editor-menu.tsx
@@ -6,7 +6,8 @@ const itemProps = {
 	returnToGallery: { symbol: "#Gallery Symbol", short: "Gallery", full: "Return to Gallery" },
 	saveDraft: { symbol: "#Save Symbol", short: "Save", full: "Save Draft" },
 	loadDraft: { symbol: "#Load Symbol", short: "Load", full: "Load Draft" },
-	submitDraft: { symbol: "#Submit Symbol", short: "Submit", full: "Submit Draft" }
+	downloadTheme: { symbol: "#Cloud Symbol", short: "Download", full: "Download Preview" },
+	submitDraft: { symbol: "#Submit Symbol", short: "Submit", full: "Submit Theme" }
 }
 
 interface Props {}
@@ -17,6 +18,7 @@ const ThemeEditorMenu: FunctionComponent<Props> = props => {
 			<MenuItem symbol={itemProps.returnToGallery.symbol} text={itemProps.returnToGallery} href="/themes#gallery" />
 			<MenuItem symbol={itemProps.saveDraft.symbol} text={itemProps.saveDraft} href="#" disabled />
 			<MenuItem symbol={itemProps.loadDraft.symbol} text={itemProps.loadDraft} href="#" disabled />
+			<MenuItem symbol={itemProps.downloadTheme.symbol} text={itemProps.downloadTheme} href="#" disabled />
 			<MenuItem symbol={itemProps.submitDraft.symbol} text={itemProps.submitDraft} href="#" disabled />
 		</Menu>
 	)
