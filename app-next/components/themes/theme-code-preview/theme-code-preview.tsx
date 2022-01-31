536e000cf1dd0bc53a8849073771bd2f42d27d49
August Saint Freytag <night@apricummedia.com>
2022-01-31 11:37:17 +0100

    Restructure theme code preview inner comps., displace code to own comp.

--- app-next/components/themes/theme-code-preview/theme-code-preview.tsx
+++ app-next/components/themes/theme-code-preview/theme-code-preview.tsx
@@ -2,12 +2,55 @@ import { CSSProperties, FunctionComponent } from "react"
 import FauxWindow from "~/components/faux-window/faux-window"
 import { tokenizedStringByLines } from "~/components/themes/theme-code-preview/functions/tokenized-string-line-split"
 import { colorFromIntermediateTheme, ThemeFormatKey } from "~/components/themes/theme-code-preview/library/theme-format-key"
-import { TokenizedString } from "~/components/themes/theme-code-preview/library/tokenized-string"
+import { SyntaxToken, TokenizedString } from "~/components/themes/theme-code-preview/library/tokenized-string"
 import { PropsWithClassName } from "~/types/props"
 import { className } from "~/utils/class-names/class-name"
 import { Color } from "~/utils/colors/models/color"
 import { IntermediateTheme } from "~/utils/themes/library/intermediate-theme"
 import styles from "./theme-code-preview.module.sass"
+
+// Code Components
+
+interface CodeProps {
+	theme: IntermediateTheme
+	tokensByLine: SyntaxToken[][]
+}
+
+const Code: FunctionComponent<CodeProps> = props => {
+	const { theme, tokensByLine } = props
+	const numberOfLines = tokensByLine.length
+	const lineNumberPadding = String(numberOfLines).length
+
+	const formattedLineIndex = (lineIndex: number) => String(lineIndex + 1).padStart(lineNumberPadding, " ")
+
+	return (
+		<code>
+			{tokensByLine.map((tokens, lineIndex) => (
+				<div className={styles.line} key={`code-line-${lineIndex}`}>
+					<div className={styles.lineNumber}>{formattedLineIndex(lineIndex)}</div>
+					<div className={styles.lineContent}>
+						{tokens.map((token, tokenIndex) => {
+							const kind = token.kind ?? ThemeFormatKey.Foreground
+							const word = token.word
+							const color = theme ? colorFromIntermediateTheme(theme, kind) : Color.black
+							const lineStyle: CSSProperties = { color: color.hex }
+
+							return (
+								<span key={`code-${lineIndex}-${tokenIndex}-${kind}`} style={lineStyle}>
+									{word}
+								</span>
+							)
+						})}
+					</div>
+				</div>
+			))}
+		</code>
+	)
+}
+
+const CodeFallback: FunctionComponent = () => <code>Preview not available for this theme package.</code>
+
+// Main Component
 
 interface Props extends PropsWithClassName {
 	theme?: IntermediateTheme
@@ -21,7 +64,7 @@ const ThemeCodePreview: FunctionComponent<Props> = props => {
 		return (
 			<section className={className(styles.block, props.className)}>
 				<FauxWindow className={styles.window} controls>
-					<code>Preview not available for this theme package.</code>
+					<CodeFallback />
 				</FauxWindow>
 			</section>
 		)
@@ -29,36 +72,12 @@ const ThemeCodePreview: FunctionComponent<Props> = props => {
 
 	const tokenizedString = props.content ?? new TokenizedString([])
 	const tokensByLine = tokenizedStringByLines(tokenizedString)
-
-	const numberOfLines = tokensByLine.length
-	const lineNumberPadding = String(numberOfLines).length
-	const formattedLineIndex = (lineIndex: number) => String(lineIndex + 1).padStart(lineNumberPadding, " ")
 	const backgroundColor = colorFromIntermediateTheme(theme, ThemeFormatKey.Background)
 
 	return (
 		<section className={className(styles.block, props.className)}>
 			<FauxWindow className={styles.window} background={backgroundColor.rgb} controls>
-				<code>
-					{tokensByLine.map((tokens, lineIndex) => (
-						<div className={styles.line} key={`code-line-${lineIndex}`}>
-							<div className={styles.lineNumber}>{formattedLineIndex(lineIndex)}</div>
-							<div className={styles.lineContent}>
-								{tokens.map((token, tokenIndex) => {
-									const kind = token.kind ?? ThemeFormatKey.Foreground
-									const word = token.word
-									const color = theme ? colorFromIntermediateTheme(theme, kind) : Color.black
-									const lineStyle: CSSProperties = { color: color.hex }
-
-									return (
-										<span key={`code-${lineIndex}-${tokenIndex}-${kind}`} style={lineStyle}>
-											{word}
-										</span>
-									)
-								})}
-							</div>
-						</div>
-					))}
-				</code>
+				<Code theme={theme} tokensByLine={tokensByLine} />
 			</FauxWindow>
 		</section>
 	)
