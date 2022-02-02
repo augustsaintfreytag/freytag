5227cd3df9e770b153f2d44a0e87792ae6a3ffbe
August Saint Freytag <night@apricummedia.com>
2022-02-02 13:37:32 +0100

    Add new tokenized string function for counting number of lines

--- app-next/components/themes/theme-code-preview/functions/tokenized-string-truncate.ts
+++ app-next/components/themes/theme-code-preview/functions/tokenized-string-truncate.ts
@@ -1,5 +1,18 @@
 import { SyntaxToken, TokenizedString } from "../library/tokenized-string"
 
+export function numberOfLinesInTokenizedString(tokenizedString: TokenizedString): number {
+	let numberOfLines = 0
+	const newLineWord = SyntaxToken.newLine.word
+
+	for (const token of tokenizedString.tokens) {
+		if (token.word === newLineWord) {
+			numberOfLines++
+		}
+	}
+
+	return numberOfLines
+}
+
 export function truncatedTokenizedString(tokenizedString: TokenizedString, maxNumberOfLines: number): TokenizedString {
 	const tokens: SyntaxToken[] = []
 	const newLineWord = SyntaxToken.newLine.word
