006398e50b627d7b18ecdc532cb72c02b69a576a
August Saint Freytag <night@apricummedia.com>
2022-02-02 13:14:17 +0100

    Redefine "SyntaxToken" type as class, update tokenized string presets
    Establish presets for various tokens (space, newLine, …) in module.

--- app-next/components/themes/theme-code-preview/library/tokenized-string.ts
+++ app-next/components/themes/theme-code-preview/library/tokenized-string.ts
@@ -7,12 +7,54 @@ export type SyntaxTokenValue = {
 	kind?: ThemeFormatKey
 }
 
+export class SyntaxToken {
+	word: string
+	kind?: ThemeFormatKey
+
+	// Init
+
+	constructor(word: string, kind?: ThemeFormatKey) {
+		this.word = word
+		this.kind = kind
+	}
+
+	static fromValue(value: SyntaxTokenValue): SyntaxToken {
+		return new SyntaxToken(value.word, value.kind)
+	}
+
+	// Presets
+
+	static get space(): SyntaxToken {
+		return new SyntaxToken(" ")
+	}
+
+	static get indent(): SyntaxToken {
+		return new SyntaxToken("    ")
+	}
+
+	static get newLine(): SyntaxToken {
+		return new SyntaxToken("\n")
+	}
+}
+
 // String
 
 export class TokenizedString {
 	tokens: SyntaxToken[] = []
 
+	// Init
+
 	constructor(tokens: SyntaxToken[]) {
 		this.tokens = tokens
+	}
+
+	// Presest
+
+	static get newLine(): TokenizedString {
+		return new TokenizedString([SyntaxToken.newLine])
+	}
+
+	static get newParagraph(): TokenizedString {
+		return new TokenizedString([SyntaxToken.newLine, SyntaxToken.newLine])
 	}
 }
