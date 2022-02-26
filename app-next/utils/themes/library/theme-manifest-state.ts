f5e017e5ea21e1778410bb09949a759c56d18c64
August Saint Freytag <night@apricummedia.com>
2022-02-26 16:50:41 +0100

    Introduce theme manifest state with associated values
    Define accompanying hook to manage and safely mutate manifest state.

---
+++ app-next/utils/themes/library/theme-manifest-state.ts
@@ -0,0 +1,22 @@
+import { ThemeManifest } from "~/utils/themes/library/theme-manifest"
+
+export interface ThemeManifestKindNone {
+	kind: ThemeManifestStateKind.None
+}
+
+export interface ThemeManifestKindPending {
+	kind: ThemeManifestStateKind.Pending
+}
+
+export interface ThemeManifestKindGenerated {
+	kind: ThemeManifestStateKind.Generated
+	manifest: ThemeManifest
+}
+
+export type ThemeManifestState = ThemeManifestKindNone | ThemeManifestKindPending | ThemeManifestKindGenerated
+
+export enum ThemeManifestStateKind {
+	None,
+	Pending,
+	Generated
+}
