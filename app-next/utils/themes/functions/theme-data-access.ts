198c50d26be19d785955540a0bee03b53a4704ce
August Saint Freytag <night@apricummedia.com>
2022-03-03 10:41:14 +0100

    Add performance measurement to theme data access

--- app-next/utils/themes/functions/theme-data-access.ts
+++ app-next/utils/themes/functions/theme-data-access.ts
@@ -1,6 +1,12 @@
+import { performanceMeasureDuration, startPerformanceMeasure, stopPerformanceMeasure } from "~/utils/performance/performance"
 import { ThemeGenerationProperties } from "~/utils/themes/library/theme-generation-properties"
 import { ThemeManifest } from "~/utils/themes/library/theme-manifest"
 import { UUID } from "~/utils/uuid/uuid"
+
+enum PerformanceKey {
+	ReadThemeManifest = "read-theme-manifest-via-api",
+	GenerateThemes = "generate-themes-via-api"
+}
 
 // Functionality to access the self-exposed API at `/api/themes`
 
@@ -12,6 +18,8 @@ export async function generateThemeViaApi(properties: ThemeGenerationProperties)
 		colors: properties.colors.map(color => color.hex)
 	}
 
+	startPerformanceMeasure(PerformanceKey.GenerateThemes)
+
 	const response = await fetch("/api/themes", {
 		method: "PUT",
 		headers: {
@@ -20,7 +28,10 @@ export async function generateThemeViaApi(properties: ThemeGenerationProperties)
 		body: JSON.stringify(themeProperties)
 	})
 
+	stopPerformanceMeasure(PerformanceKey.GenerateThemes)
+
 	try {
+		console.log(`Generated themes via API in ${performanceMeasureDuration(PerformanceKey.GenerateThemes)}.`)
 		return (await response.json()) as ThemeManifest
 	} catch (error) {
 		throw new Error(`Could not decode generate theme response as valid manifest. ${String(error)}`)
