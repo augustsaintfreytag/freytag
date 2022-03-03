912806376eeb3ced225f3d398c9e893726869488
August Saint Freytag <night@apricummedia.com>
2022-03-03 10:05:02 +0100

    Make id property optional for theme generation

--- app-next/utils/themes/library/theme-generation-properties.ts
+++ app-next/utils/themes/library/theme-generation-properties.ts
@@ -2,7 +2,7 @@ import { Color } from "~/utils/colors/models/color"
 import { UUID } from "~/utils/uuid/uuid"
 
 export interface ThemeGenerationProperties {
-	id: UUID
+	id?: UUID
 	name: string
 	description: string
 	colors: Color[]
