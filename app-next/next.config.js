a2071f67a9b2c5c7c960e8e9d5ed4f3c63ef6055
August Saint Freytag <night@apricummedia.com>
2022-03-13 13:50:08 +0100

    Change polling and aggregate for Next watch mode in development

--- app-next/next.config.js
+++ app-next/next.config.js
@@ -10,8 +10,9 @@ module.exports = {
 	},
 	webpackDevMiddleware(config) {
 		config.watchOptions = {
-			poll: 500,
-			aggregateTimeout: 250
+			poll: 1000,
+			aggregateTimeout: 250,
+			ignored: "**/node_modules/"
 		}
 
 		return config
