9a9323f9cf639b4a133cb52958050eec85eea6fd
August Saint Freytag <night@apricummedia.com>
2022-03-29 10:35:19 +0200

    Remove duplicate Next type declarations, update props definitions

--- app-next/types/props.ts
+++ app-next/types/props.ts
@@ -1,16 +1,7 @@
-import { ReactElement, ReactNode, ReactNodeArray, ReactPortal, ReactText } from "react"
+import { ReactElement, ReactNode, ReactPortal, ReactText } from "react"
 import { URL } from "~/utils/routing/library/url"
 
-export type AnyChild =
-	| string
-	| number
-	| boolean
-	| {}
-	| ReactElement<any, string | React.JSXElementConstructor<any>>
-	| ReactNodeArray
-	| ReactPortal
-	| null
-	| undefined
+export type AnyChild = string | number | boolean | {} | ReactElement<any, string | React.JSXElementConstructor<any>> | ReactPortal | null | undefined
 
 export type PropsWithAnyChildren = { children?: ReactNode | ReactNode[] }
 export type PropsWithAnyTextChildren = { children?: ReactText }
