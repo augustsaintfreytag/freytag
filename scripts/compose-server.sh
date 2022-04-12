3fd98d6ce3b257e9960301910652bcdf77cd4ba9
August Saint Freytag <night@apricummedia.com>
2022-04-12 12:38:04 +0200

    Revise compose scripts, use POSIX shell, add quoting for paths

--- scripts/compose-server.sh
+++ scripts/compose-server.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env sh
+# shellcheck disable=SC2068
 
 BASE_DIR=$(dirname "$0")
 ROOT_DIR=$(realpath "$BASE_DIR/..")
 
-docker compose --file "$ROOT_DIR/server/docker/docker-compose.yml" --project-name freytag --project-directory "$ROOT_DIR" --env-file "$ROOT_DIR/server/docker/env/parameters.production.env" $@
+DOCKER_DIR="$ROOT_DIR/server/docker"
+DOCKER_ENV="$DOCKER_DIR/env/parameters.production.env"
+
+docker compose --file "$ROOT_DIR/server/docker/docker-compose.yml" --project-name freytag --project-directory "$ROOT_DIR" --env-file "$DOCKER_ENV" $@
--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,4 +1,5 @@
-#! /usr/bin/env zsh
+#! /usr/bin/env sh
+# shellcheck disable=SC2068
 
 BASE_DIR=$(dirname "$0")
 ROOT_DIR=$(realpath "$BASE_DIR/..")
@@ -9,21 +10,24 @@ DOCKER_DIR="$ROOT_DIR/server/docker"
 DOCKER_ENV="$DOCKER_DIR/env/parameters.local.env"
 
 set -a
-source "$DOCKER_ENV"
+	# shellcheck source=../server/docker/env/parameters.local.env
+	. "$DOCKER_ENV"
 set +a
 
 # Modifier: Node Inspector
 
-if [[ $DEBUG == "1" ]]; then
-	APP_STARTUP_CMD="yarn dev-inspect"
+if [ "$DEBUG" = "1" ]
+then
+	export APP_STARTUP_CMD="yarn dev-inspect"
 fi
 
 # Modifier: Simulated Production
 
-if [[ $DEBUG_PRODUCTION == "1" ]]; then
-	APP_STARTUP_CMD="yarn live"
+if [ "$DEBUG_PRODUCTION" = "1" ]
+then
+	export APP_STARTUP_CMD="yarn live"
 fi
 
 # Execution
 
-docker compose --file $DOCKER_DIR/docker-compose.yml --project-name=freytag --project-directory $ROOT_DIR $@
+docker compose --file "$DOCKER_DIR/docker-compose.yml" --project-name=freytag --project-directory "$ROOT_DIR" $@
